205e8fde08a8ab7ae8781c237468527a7ff254dd
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-01 09:26:10 +0000

    solve: 4A - Watermelon

---
+++ 2021/2021-10-01/4A_Watermelon.cpp
@@ -0,0 +1,5 @@
+// Problem: 4A - Watermelon
+// URL: https://codeforces.com/problemset/problem/4/A
+// Date: 2021-10-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/4/submission/130448657
