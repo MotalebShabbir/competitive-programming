6cf9c882c8e849c30f778957dc3059582952ef9d
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-01 09:37:44 +0000

    solve: 71A - Way Too Long Words

---
+++ 2021/2021-10-01/71A_Way_Too_Long_Words.cpp
@@ -0,0 +1,5 @@
+// Problem: 71A - Way Too Long Words
+// URL: https://codeforces.com/problemset/problem/71/A
+// Date: 2021-10-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/71/submission/130449460
