a1506fc219f3caba6f664a20a6456f6cf84c5603
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-01 10:02:59 +0000

    solve: 231A - Team

---
+++ 2021/2021-10-01/231A_Team.cpp
@@ -0,0 +1,5 @@
+// Problem: 231A - Team
+// URL: https://codeforces.com/problemset/problem/231/A
+// Date: 2021-10-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/231/submission/130451240
