0d3a4c1dd9c823178076490045e0cedd7cea4c0b
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-02 11:42:13 +0000

    solve: 158A - Next Round

---
+++ 2021/2021-10-02/158A_Next_Round.cpp
@@ -0,0 +1,5 @@
+// Problem: 158A - Next Round
+// URL: https://codeforces.com/problemset/problem/158/A
+// Date: 2021-10-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/158/submission/130556845
