a278821658b90456db9ead37bd7e4759c442de84
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-02 12:45:50 +0000

    solve: 282A - Bit++

---
+++ 2021/2021-10-02/282A_Bit++.cpp
@@ -0,0 +1,5 @@
+// Problem: 282A - Bit++
+// URL: https://codeforces.com/problemset/problem/282/A
+// Date: 2021-10-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/282/submission/130561954
