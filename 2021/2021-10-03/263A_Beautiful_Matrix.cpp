2a486334b8912c340bcca3c8c7e1456f47047bb5
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-03 03:40:40 +0000

    solve: 263A - Beautiful Matrix

---
+++ 2021/2021-10-03/263A_Beautiful_Matrix.cpp
@@ -0,0 +1,5 @@
+// Problem: 263A - Beautiful Matrix
+// URL: https://codeforces.com/problemset/problem/263/A
+// Date: 2021-10-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/263/submission/130611673
