c4177b182491376a58e5106f239e6d401cdf4499
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-04 13:36:35 +0000

    solve: 112A - Petya and Strings

---
+++ 2021/2021-10-04/112A_Petya_and_Strings.cpp
@@ -0,0 +1,5 @@
+// Problem: 112A - Petya and Strings
+// URL: https://codeforces.com/problemset/problem/112/A
+// Date: 2021-10-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/112/submission/130805121
