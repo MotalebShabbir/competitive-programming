08db7c11bb8ded8dd2c7534fcf28b825bd68c02f
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-05 12:40:47 +0000

    solve: 339A - Helpful Maths

---
+++ 2021/2021-10-05/339A_Helpful_Maths.cpp
@@ -0,0 +1,5 @@
+// Problem: 339A - Helpful Maths
+// URL: https://codeforces.com/problemset/problem/339/A
+// Date: 2021-10-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/339/submission/130889839
