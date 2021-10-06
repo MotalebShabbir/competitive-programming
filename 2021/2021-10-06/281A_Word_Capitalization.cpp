244572e99a717a9503e8070387b932b6da4b8c9a
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-06 14:42:46 +0000

    solve: 281A - Word Capitalization

---
+++ 2021/2021-10-06/281A_Word_Capitalization.cpp
@@ -0,0 +1,5 @@
+// Problem: 281A - Word Capitalization
+// URL: https://codeforces.com/problemset/problem/281/A
+// Date: 2021-10-06
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/281/submission/130987944
