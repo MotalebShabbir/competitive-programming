334cb18e3dafd78691b09e280dcbaccbb6b2f337
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-07 11:35:14 +0000

    solve: 266A - Stones on the Table

---
+++ 2021/2021-10-07/266A_Stones_on_the_Table.cpp
@@ -0,0 +1,5 @@
+// Problem: 266A - Stones on the Table
+// URL: https://codeforces.com/problemset/problem/266/A
+// Date: 2021-10-07
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/266/submission/131059885
