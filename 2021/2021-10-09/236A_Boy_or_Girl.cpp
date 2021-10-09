349e373ab5b2d271d6ffb850289b035ab9e6740b
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-09 02:10:52 +0000

    solve: 236A - Boy or Girl

---
+++ 2021/2021-10-09/236A_Boy_or_Girl.cpp
@@ -0,0 +1,5 @@
+// Problem: 236A - Boy or Girl
+// URL: https://codeforces.com/problemset/problem/236/A
+// Date: 2021-10-09
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/236/submission/131257353
