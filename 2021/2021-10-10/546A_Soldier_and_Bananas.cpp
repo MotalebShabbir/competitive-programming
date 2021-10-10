109ad136ba44ae6d2c5584240912f84ab0edff6c
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-10 06:39:53 +0000

    solve: 546A - Soldier and Bananas

---
+++ 2021/2021-10-10/546A_Soldier_and_Bananas.cpp
@@ -0,0 +1,5 @@
+// Problem: 546A - Soldier and Bananas
+// URL: https://codeforces.com/problemset/problem/546/A
+// Date: 2021-10-10
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/546/submission/131384171
