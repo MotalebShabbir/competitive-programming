3274330a372dfd949557314d2d949344e88013cc
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-11 11:02:20 +0000

    solve: 791A - Bear and Big Brother

---
+++ 2021/2021-10-11/791A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,5 @@
+// Problem: 791A - Bear and Big Brother
+// URL: https://codeforces.com/problemset/problem/791/A
+// Date: 2021-10-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/791/submission/131540406
