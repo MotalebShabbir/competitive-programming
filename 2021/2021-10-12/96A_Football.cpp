85510f1a56a6a2678fa18037e590739bdaf074aa
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-12 04:35:44 +0000

    solve: 96A - Football

---
+++ 2021/2021-10-12/96A_Football.cpp
@@ -0,0 +1,5 @@
+// Problem: 96A - Football
+// URL: https://codeforces.com/problemset/problem/96/A
+// Date: 2021-10-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/96/submission/131605785
