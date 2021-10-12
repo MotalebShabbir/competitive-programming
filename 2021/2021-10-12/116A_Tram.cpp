1e0f3976d35f713122bc3a7eb01f26a2cbe5f196
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-12 04:02:26 +0000

    solve: 116A - Tram

---
+++ 2021/2021-10-12/116A_Tram.cpp
@@ -0,0 +1,5 @@
+// Problem: 116A - Tram
+// URL: https://codeforces.com/problemset/problem/116/A
+// Date: 2021-10-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/116/submission/131604391
