31dbd2681c8cc34c6883835526be1ad59269434b
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-12 03:55:57 +0000

    solve: 266B - Queue at the School

---
+++ 2021/2021-10-12/266B_Queue_at_the_School.cpp
@@ -0,0 +1,5 @@
+// Problem: 266B - Queue at the School
+// URL: https://codeforces.com/problemset/problem/266/B
+// Date: 2021-10-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/266/submission/131604130
