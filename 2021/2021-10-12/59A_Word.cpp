0b68698e5da8451c172e8f02eba76fdf87693d6e
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-12 03:11:39 +0000

    solve: 59A - Word

---
+++ 2021/2021-10-12/59A_Word.cpp
@@ -0,0 +1,5 @@
+// Problem: 59A - Word
+// URL: https://codeforces.com/problemset/problem/59/A
+// Date: 2021-10-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/59/submission/131602352
