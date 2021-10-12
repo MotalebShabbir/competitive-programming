584136c9dd70b957b07d7d5a62e1c6705d1c8889
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-12 03:00:21 +0000

    solve: 977A - Wrong Subtraction

---
+++ 2021/2021-10-12/977A_Wrong_Subtraction.cpp
@@ -0,0 +1,5 @@
+// Problem: 977A - Wrong Subtraction
+// URL: https://codeforces.com/problemset/problem/977/A
+// Date: 2021-10-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/977/submission/131601918
