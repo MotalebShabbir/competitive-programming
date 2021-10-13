b3e9cd77fc6cc982fefc962af87c2029205a39b4
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-13 05:18:41 +0000

    solve: 133A - HQ9+

---
+++ 2021/2021-10-13/133A_HQ9+.cpp
@@ -0,0 +1,5 @@
+// Problem: 133A - HQ9+
+// URL: https://codeforces.com/problemset/problem/133/A
+// Date: 2021-10-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/133/submission/131699325
