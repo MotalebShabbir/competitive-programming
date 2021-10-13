b04f861e1ae47c6dedf24a56313202a18aabe62a
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-13 05:55:01 +0000

    solve: 318A - Even Odds

---
+++ 2021/2021-10-13/318A_Even_Odds.cpp
@@ -0,0 +1,5 @@
+// Problem: 318A - Even Odds
+// URL: https://codeforces.com/problemset/problem/318/A
+// Date: 2021-10-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/318/submission/131701641
