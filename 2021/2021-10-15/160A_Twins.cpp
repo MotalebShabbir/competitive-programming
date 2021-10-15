16d0cedaa0ef0d67fb4d4c278719439702270c36
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-15 15:48:13 +0000

    solve: 160A - Twins

---
+++ 2021/2021-10-15/160A_Twins.cpp
@@ -0,0 +1,5 @@
+// Problem: 160A - Twins
+// URL: https://codeforces.com/problemset/problem/160/A
+// Date: 2021-10-15
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/160/submission/132048772
