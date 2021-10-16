280e96224dca30980f07be08ae55c0296e427887
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-16 12:47:19 +0000

    solve: 580A - Kefa and First Steps

---
+++ 2021/2021-10-16/580A_Kefa_and_First_Steps.cpp
@@ -0,0 +1,5 @@
+// Problem: 580A - Kefa and First Steps
+// URL: https://codeforces.com/problemset/problem/580/A
+// Date: 2021-10-16
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/580/submission/132128415
