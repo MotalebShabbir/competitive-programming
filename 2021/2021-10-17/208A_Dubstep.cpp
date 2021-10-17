aa5a83a82391b9db7fb20813bc198978ab94512f
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-17 15:25:10 +0000

    solve: 208A - Dubstep

---
+++ 2021/2021-10-17/208A_Dubstep.cpp
@@ -0,0 +1,5 @@
+// Problem: 208A - Dubstep
+// URL: https://codeforces.com/problemset/problem/208/A
+// Date: 2021-10-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/208/submission/132270259
