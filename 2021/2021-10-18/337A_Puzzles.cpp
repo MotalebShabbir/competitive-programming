b28b4485535a20fb4c59f42ffbfd53306cef7e7e
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-18 02:52:07 +0000

    solve: 337A - Puzzles

---
+++ 2021/2021-10-18/337A_Puzzles.cpp
@@ -0,0 +1,5 @@
+// Problem: 337A - Puzzles
+// URL: https://codeforces.com/problemset/problem/337/A
+// Date: 2021-10-18
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/337/submission/132301758
