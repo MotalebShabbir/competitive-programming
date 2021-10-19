2977dfd6cb0f92213e19c4218270d518ca95d3fc
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-19 02:04:38 +0000

    solve: 451A - Game With Sticks

---
+++ 2021/2021-10-19/451A_Game_With_Sticks.cpp
@@ -0,0 +1,5 @@
+// Problem: 451A - Game With Sticks
+// URL: https://codeforces.com/problemset/problem/451/A
+// Date: 2021-10-19
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/451/submission/132393816
