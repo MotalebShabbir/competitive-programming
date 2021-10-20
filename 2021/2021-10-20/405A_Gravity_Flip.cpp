d5ad938b46c1fcb3240558beb9826f3c250c5dfb
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-20 14:38:21 +0000

    solve: 405A - Gravity Flip

---
+++ 2021/2021-10-20/405A_Gravity_Flip.cpp
@@ -0,0 +1,5 @@
+// Problem: 405A - Gravity Flip
+// URL: https://codeforces.com/problemset/problem/405/A
+// Date: 2021-10-20
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/405/submission/132539727
