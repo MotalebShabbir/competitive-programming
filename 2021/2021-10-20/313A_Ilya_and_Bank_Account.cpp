2c33bbb4cbaf40d7ecb237707b4bf44aa570bd2d
MotalebShabbir <motalebshabbir@gmail.com>
2021-10-20 04:40:00 +0000

    solve: 313A - Ilya and Bank Account

---
+++ 2021/2021-10-20/313A_Ilya_and_Bank_Account.cpp
@@ -0,0 +1,5 @@
+// Problem: 313A - Ilya and Bank Account
+// URL: https://codeforces.com/problemset/problem/313/A
+// Date: 2021-10-20
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/313/submission/132490986
