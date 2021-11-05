f0f9f66827790ef77295d77e822552d38add8b6f
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-05 06:08:55 +0000

    solve: 1475A - Odd Divisor

---
+++ 2021/2021-11-05/1475A_Odd_Divisor.cpp
@@ -0,0 +1,5 @@
+// Problem: 1475A - Odd Divisor
+// URL: https://codeforces.com/problemset/problem/1475/A
+// Date: 2021-11-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1475/submission/134366447
