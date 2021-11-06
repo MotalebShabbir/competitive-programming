fc64f7cb928c7b74fc7c2f896716b194593bd18f
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-06 12:05:57 +0000

    solve: 1374B - Multiply by 2, divide by 6

---
+++ 2021/2021-11-06/1374B_Multiply_by_2,_divide_by_6.cpp
@@ -0,0 +1,5 @@
+// Problem: 1374B - Multiply by 2, divide by 6
+// URL: https://codeforces.com/problemset/problem/1374/B
+// Date: 2021-11-06
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1374/submission/134489920
