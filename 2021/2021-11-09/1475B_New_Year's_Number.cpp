29d8723039b9330e82dd03efe27e08dfb41aba62
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-09 11:53:33 +0000

    solve: 1475B - New Year's Number

---
+++ 2021/2021-11-09/1475B_New_Year's_Number.cpp
@@ -0,0 +1,5 @@
+// Problem: 1475B - New Year's Number
+// URL: https://codeforces.com/problemset/problem/1475/B
+// Date: 2021-11-09
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1475/submission/134752442
