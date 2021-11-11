43d341f4d1ecbed396208c0799e2a0aedbcdfa86
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-11 12:50:51 +0000

    solve: 1335B - Construct the String

---
+++ 2021/2021-11-11/1335B_Construct_the_String.cpp
@@ -0,0 +1,5 @@
+// Problem: 1335B - Construct the String
+// URL: https://codeforces.com/problemset/problem/1335/B
+// Date: 2021-11-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1335/submission/134984423
