e3dc00531599e32cc93dbbeb10fed3fdd900ae2f
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-12 06:14:02 +0000

    solve: 110A - Nearly Lucky Number

---
+++ 2021/2021-11-12/110A_Nearly_Lucky_Number.cpp
@@ -0,0 +1,5 @@
+// Problem: 110A - Nearly Lucky Number
+// URL: https://codeforces.com/problemset/problem/110/A
+// Date: 2021-11-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/110/submission/135060293
