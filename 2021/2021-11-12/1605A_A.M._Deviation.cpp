0831113427eb76995d10d0db2e1f826a06db29ef
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-12 15:59:19 +0000

    solve: 1605A - A.M. Deviation

---
+++ 2021/2021-11-12/1605A_A.M._Deviation.cpp
@@ -0,0 +1,5 @@
+// Problem: 1605A - A.M. Deviation
+// URL: https://codeforces.com/problemset/problem/1605/A
+// Date: 2021-11-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1605/submission/135154833
