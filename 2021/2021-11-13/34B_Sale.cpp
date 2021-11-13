59717d79795fc181ad1df3a47b84e01959e1cad0
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-13 01:47:01 +0000

    solve: 34B - Sale

---
+++ 2021/2021-11-13/34B_Sale.cpp
@@ -0,0 +1,5 @@
+// Problem: 34B - Sale
+// URL: https://codeforces.com/problemset/problem/34/B
+// Date: 2021-11-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/34/submission/135205919
