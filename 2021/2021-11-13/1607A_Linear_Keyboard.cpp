40bc7e38f145e42461fb4e3cb68e1f0ef9214ee4
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-13 07:01:49 +0000

    solve: 1607A - Linear Keyboard

---
+++ 2021/2021-11-13/1607A_Linear_Keyboard.cpp
@@ -0,0 +1,5 @@
+// Problem: 1607A - Linear Keyboard
+// URL: https://codeforces.com/problemset/problem/1607/A
+// Date: 2021-11-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1607/submission/135227742
