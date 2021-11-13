20b2fd4e507f4e9d0fe04b8b014b14ae7c68a609
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-13 11:57:57 +0000

    solve: 474A - Keyboard

---
+++ 2021/2021-11-13/474A_Keyboard.cpp
@@ -0,0 +1,5 @@
+// Problem: 474A - Keyboard
+// URL: https://codeforces.com/problemset/problem/474/A
+// Date: 2021-11-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/474/submission/135255688
