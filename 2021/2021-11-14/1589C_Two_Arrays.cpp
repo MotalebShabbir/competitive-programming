e799d48e80b5b18a7fae50e0e32370675f78e6c9
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-14 07:58:34 +0000

    solve: 1589C - Two Arrays

---
+++ 2021/2021-11-14/1589C_Two_Arrays.cpp
@@ -0,0 +1,5 @@
+// Problem: 1589C - Two Arrays
+// URL: https://codeforces.com/problemset/problem/1589/C
+// Date: 2021-11-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1589/submission/135386559
