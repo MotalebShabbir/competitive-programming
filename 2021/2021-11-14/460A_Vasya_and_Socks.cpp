5bc4522b5e45e3fe8feb1de1891d48c98944c56d
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-14 02:41:47 +0000

    solve: 460A - Vasya and Socks

---
+++ 2021/2021-11-14/460A_Vasya_and_Socks.cpp
@@ -0,0 +1,5 @@
+// Problem: 460A - Vasya and Socks
+// URL: https://codeforces.com/problemset/problem/460/A
+// Date: 2021-11-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/460/submission/135334551
