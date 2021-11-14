9e9667507322405f1ba5bbecad557be4459b6393
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-14 06:36:51 +0000

    solve: 1589B - Coloring Rectangles

---
+++ 2021/2021-11-14/1589B_Coloring_Rectangles.cpp
@@ -0,0 +1,5 @@
+// Problem: 1589B - Coloring Rectangles
+// URL: https://codeforces.com/problemset/problem/1589/B
+// Date: 2021-11-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1589/submission/135358443
