72426fccdcff9cc5ceffefffdfcb33db61d85c8f
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-15 02:49:54 +0000

    solve: 118A - String Task

---
+++ 2021/2021-11-15/118A_String_Task.cpp
@@ -0,0 +1,5 @@
+// Problem: 118A - String Task
+// URL: https://codeforces.com/problemset/problem/118/A
+// Date: 2021-11-15
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/118/submission/135553745
