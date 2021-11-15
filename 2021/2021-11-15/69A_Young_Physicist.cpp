5529dd4d4884f93112331608e1559d7b2468e877
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-15 12:44:35 +0000

    solve: 69A - Young Physicist

---
+++ 2021/2021-11-15/69A_Young_Physicist.cpp
@@ -0,0 +1,5 @@
+// Problem: 69A - Young Physicist
+// URL: https://codeforces.com/problemset/problem/69/A
+// Date: 2021-11-15
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/69/submission/135734264
