e5402b0f6b208753b7c4e457b1b91310d9b32887
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-17 12:43:51 +0000

    solve: 122A - Lucky Division

---
+++ 2021/2021-11-17/122A_Lucky_Division.cpp
@@ -0,0 +1,5 @@
+// Problem: 122A - Lucky Division
+// URL: https://codeforces.com/problemset/problem/122/A
+// Date: 2021-11-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/122/submission/135969923
