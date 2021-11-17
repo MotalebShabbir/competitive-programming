dc8b7585b4cc526e7678db5e093042109e9c6330
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-17 16:17:41 +0000

    solve: 479A - Expression

---
+++ 2021/2021-11-17/479A_Expression.cpp
@@ -0,0 +1,5 @@
+// Problem: 479A - Expression
+// URL: https://codeforces.com/problemset/problem/479/A
+// Date: 2021-11-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/479/submission/135997795
