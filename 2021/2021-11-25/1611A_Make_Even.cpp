69130034bb9ead0969c27492926fc0a78ae20a80
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-25 15:24:39 +0000

    solve: 1611A - Make Even

---
+++ 2021/2021-11-25/1611A_Make_Even.cpp
@@ -0,0 +1,5 @@
+// Problem: 1611A - Make Even
+// URL: https://codeforces.com/problemset/problem/1611/A
+// Date: 2021-11-25
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1611/submission/136882213
