5120f1913bfd027d989b14b7809a2c70b8ab0e02
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-25 05:38:57 +0000

    solve: 131A - cAPS lOCK

---
+++ 2021/2021-11-25/131A_cAPS_lOCK.cpp
@@ -0,0 +1,5 @@
+// Problem: 131A - cAPS lOCK
+// URL: https://codeforces.com/problemset/problem/131/A
+// Date: 2021-11-25
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/131/submission/136795461
