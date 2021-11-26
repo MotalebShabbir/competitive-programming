5fb0f3acc02be965340dee182a947e79a6c40375
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-26 12:27:27 +0000

    solve: 1614A - Divan and a Store

---
+++ 2021/2021-11-26/1614A_Divan_and_a_Store.cpp
@@ -0,0 +1,5 @@
+// Problem: 1614A - Divan and a Store
+// URL: https://codeforces.com/problemset/problem/1614/A
+// Date: 2021-11-26
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1614/submission/137022347
