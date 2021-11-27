1bcf4a887504f9723f385266752266ae0985ca3c
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-27 02:43:43 +0000

    solve: 556A - Case of the Zeros and Ones

---
+++ 2021/2021-11-27/556A_Case_of_the_Zeros_and_Ones.cpp
@@ -0,0 +1,5 @@
+// Problem: 556A - Case of the Zeros and Ones
+// URL: https://codeforces.com/problemset/problem/556/A
+// Date: 2021-11-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/556/submission/137080055
