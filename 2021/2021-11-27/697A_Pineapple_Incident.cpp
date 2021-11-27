45de3df192fbd219a6483922381b5b07a02ab2eb
MotalebShabbir <motalebshabbir@gmail.com>
2021-11-27 12:53:16 +0000

    solve: 697A - Pineapple Incident

---
+++ 2021/2021-11-27/697A_Pineapple_Incident.cpp
@@ -0,0 +1,5 @@
+// Problem: 697A - Pineapple Incident
+// URL: https://codeforces.com/problemset/problem/697/A
+// Date: 2021-11-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/697/submission/137128830
