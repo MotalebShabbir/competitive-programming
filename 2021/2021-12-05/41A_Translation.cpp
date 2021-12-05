85e43138288d0b447de1b894db40bec68a24b6bd
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-05 05:28:05 +0000

    solve: 41A - Translation

---
+++ 2021/2021-12-05/41A_Translation.cpp
@@ -0,0 +1,5 @@
+// Problem: 41A - Translation
+// URL: https://codeforces.com/problemset/problem/41/A
+// Date: 2021-12-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/41/submission/138043710
