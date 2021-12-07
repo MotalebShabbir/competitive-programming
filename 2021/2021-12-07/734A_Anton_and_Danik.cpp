d0fa95b7c5aef0fb9702044f1a7fe616fa96e7c8
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-07 03:46:00 +0000

    solve: 734A - Anton and Danik

---
+++ 2021/2021-12-07/734A_Anton_and_Danik.cpp
@@ -0,0 +1,5 @@
+// Problem: 734A - Anton and Danik
+// URL: https://codeforces.com/problemset/problem/734/A
+// Date: 2021-12-07
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/734/submission/138308571
