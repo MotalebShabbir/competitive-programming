9ec39e5168fb9831fc1b23a2c81f742e31840cfa
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-11 05:20:44 +0000

    solve: 271A - Beautiful Year

---
+++ 2021/2021-12-11/271A_Beautiful_Year.cpp
@@ -0,0 +1,5 @@
+// Problem: 271A - Beautiful Year
+// URL: https://codeforces.com/problemset/problem/271/A
+// Date: 2021-12-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/271/submission/138699058
