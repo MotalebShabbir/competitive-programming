02221ed1210de3f6cb4674af34037ccaab8e6fca
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-12 01:35:55 +0000

    solve: 677A - Vanya and Fence

---
+++ 2021/2021-12-12/677A_Vanya_and_Fence.cpp
@@ -0,0 +1,5 @@
+// Problem: 677A - Vanya and Fence
+// URL: https://codeforces.com/problemset/problem/677/A
+// Date: 2021-12-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/677/submission/138821552
