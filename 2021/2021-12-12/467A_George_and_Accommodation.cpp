87afdb0f5e28ab213c4bcbda8bf7bd90a121c8c8
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-12 09:36:59 +0000

    solve: 467A - George and Accommodation

---
+++ 2021/2021-12-12/467A_George_and_Accommodation.cpp
@@ -0,0 +1,5 @@
+// Problem: 467A - George and Accommodation
+// URL: https://codeforces.com/problemset/problem/467/A
+// Date: 2021-12-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/467/submission/138844087
