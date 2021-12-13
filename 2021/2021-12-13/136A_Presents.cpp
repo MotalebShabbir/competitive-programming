f3aa0e70b758390e09e5557847db7cb51815d69a
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-13 06:03:29 +0000

    solve: 136A - Presents

---
+++ 2021/2021-12-13/136A_Presents.cpp
@@ -0,0 +1,5 @@
+// Problem: 136A - Presents
+// URL: https://codeforces.com/problemset/problem/136/A
+// Date: 2021-12-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/136/submission/138953805
