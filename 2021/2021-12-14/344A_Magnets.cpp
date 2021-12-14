015e705110a2859148b37331d9ce62fc2494aafb
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-14 15:49:35 +0000

    solve: 344A - Magnets

---
+++ 2021/2021-12-14/344A_Magnets.cpp
@@ -0,0 +1,5 @@
+// Problem: 344A - Magnets
+// URL: https://codeforces.com/problemset/problem/344/A
+// Date: 2021-12-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/344/submission/139279531
