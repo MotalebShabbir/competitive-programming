74e7aa0e30be6469c4e0596cb22e5ddf9c860404
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-14 12:19:21 +0000

    solve: 1030A - In Search of an Easy Problem

---
+++ 2021/2021-12-14/1030A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,5 @@
+// Problem: 1030A - In Search of an Easy Problem
+// URL: https://codeforces.com/problemset/problem/1030/A
+// Date: 2021-12-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1030/submission/139136867
