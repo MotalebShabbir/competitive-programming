730bfa6245ec5eb51bf059c75f72a7b8d2c24b23
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-15 12:03:01 +0000

    solve: 1608A - Find Array

---
+++ 2021/2021-12-15/1608A_Find_Array.cpp
@@ -0,0 +1,5 @@
+// Problem: 1608A - Find Array
+// URL: https://codeforces.com/problemset/problem/1608/A
+// Date: 2021-12-15
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1608/submission/139387232
