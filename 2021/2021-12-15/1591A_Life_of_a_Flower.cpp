be47ef180d2bc4065c4881eddf702c3ab21e446b
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-15 12:34:01 +0000

    solve: 1591A - Life of a Flower

---
+++ 2021/2021-12-15/1591A_Life_of_a_Flower.cpp
@@ -0,0 +1,5 @@
+// Problem: 1591A - Life of a Flower
+// URL: https://codeforces.com/problemset/problem/1591/A
+// Date: 2021-12-15
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1591/submission/139390499
