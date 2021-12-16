ac2cd6edbd93e85495585732575bfb4c2afa84d5
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-16 01:28:36 +0000

    solve: 255A - Greg's Workout

---
+++ 2021/2021-12-16/255A_Greg's_Workout.cpp
@@ -0,0 +1,5 @@
+// Problem: 255A - Greg's Workout
+// URL: https://codeforces.com/problemset/problem/255/A
+// Date: 2021-12-16
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/255/submission/139440909
