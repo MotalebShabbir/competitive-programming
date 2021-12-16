4203f728cb3477d83caaf37017c9bcc8e778c0e5
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-16 14:25:11 +0000

    solve: 1617B - GCD Problem

---
+++ 2021/2021-12-16/1617B_GCD_Problem.cpp
@@ -0,0 +1,5 @@
+// Problem: 1617B - GCD Problem
+// URL: https://codeforces.com/problemset/problem/1617/B
+// Date: 2021-12-16
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1617/submission/139513857
