316cc251fccaa3e1649df7c5eba9a31a586625a3
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-18 10:21:24 +0000

    solve: 486A - Calculating Function

---
+++ 2021/2021-12-18/486A_Calculating_Function.cpp
@@ -0,0 +1,5 @@
+// Problem: 486A - Calculating Function
+// URL: https://codeforces.com/problemset/problem/486/A
+// Date: 2021-12-18
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/486/submission/139728403
