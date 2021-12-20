fe9d1c18bd287b3b1eb18f77209d537553b8bbf3
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-20 03:19:43 +0000

    solve: 1618A - Polycarp and Sums of Subsequences

---
+++ 2021/2021-12-20/1618A_Polycarp_and_Sums_of_Subsequences.cpp
@@ -0,0 +1,5 @@
+// Problem: 1618A - Polycarp and Sums of Subsequences
+// URL: https://codeforces.com/problemset/problem/1618/A
+// Date: 2021-12-20
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1618/submission/139948394
