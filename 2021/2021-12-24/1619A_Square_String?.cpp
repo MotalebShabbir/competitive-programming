8248d95a45bafe8c179ba1daeec34f341d9d1a58
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-24 10:52:56 +0000

    solve: 1619A - Square String?

---
+++ 2021/2021-12-24/1619A_Square_String?.cpp
@@ -0,0 +1,5 @@
+// Problem: 1619A - Square String?
+// URL: https://codeforces.com/problemset/problem/1619/A
+// Date: 2021-12-24
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1619/submission/140428448
