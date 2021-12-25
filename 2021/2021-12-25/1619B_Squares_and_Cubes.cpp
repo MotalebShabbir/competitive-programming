dddb8feaa930060ce5c6d9f33a7c093d8d295437
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-25 02:26:43 +0000

    solve: 1619B - Squares and Cubes

---
+++ 2021/2021-12-25/1619B_Squares_and_Cubes.cpp
@@ -0,0 +1,5 @@
+// Problem: 1619B - Squares and Cubes
+// URL: https://codeforces.com/problemset/problem/1619/B
+// Date: 2021-12-25
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1619/submission/140533318
