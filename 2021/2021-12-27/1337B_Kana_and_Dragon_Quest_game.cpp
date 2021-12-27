57c83100a57ace117645c0e95f024f1e19a6a824
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-27 12:37:14 +0000

    solve: 1337B - Kana and Dragon Quest game

---
+++ 2021/2021-12-27/1337B_Kana_and_Dragon_Quest_game.cpp
@@ -0,0 +1,5 @@
+// Problem: 1337B - Kana and Dragon Quest game
+// URL: https://codeforces.com/problemset/problem/1337/B
+// Date: 2021-12-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1337/submission/140740797
