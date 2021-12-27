245de2afed65511e34215f3bcfb8da550afa5132
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-27 16:51:14 +0000

    solve: 230A - Dragons

---
+++ 2021/2021-12-27/230A_Dragons.cpp
@@ -0,0 +1,5 @@
+// Problem: 230A - Dragons
+// URL: https://codeforces.com/problemset/problem/230/A
+// Date: 2021-12-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/230/submission/140823890
