38258e79217807af9bcfb20030d1ca739fee4c97
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-27 01:03:45 +0000

    solve: 61A - Ultra-Fast Mathematician

---
+++ 2021/2021-12-27/61A_Ultra-Fast_Mathematician.cpp
@@ -0,0 +1,5 @@
+// Problem: 61A - Ultra-Fast Mathematician
+// URL: https://codeforces.com/problemset/problem/61/A
+// Date: 2021-12-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/61/submission/140699565
