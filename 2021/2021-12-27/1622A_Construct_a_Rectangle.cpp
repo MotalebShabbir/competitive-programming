3b59fc9e47904e2db0bc92c52c41111deb941d14
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-27 15:29:44 +0000

    solve: 1622A - Construct a Rectangle

---
+++ 2021/2021-12-27/1622A_Construct_a_Rectangle.cpp
@@ -0,0 +1,5 @@
+// Problem: 1622A - Construct a Rectangle
+// URL: https://codeforces.com/problemset/problem/1622/A
+// Date: 2021-12-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1622/submission/140790703
