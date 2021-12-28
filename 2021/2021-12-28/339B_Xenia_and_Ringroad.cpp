9aded1b202070f2f9d36a4a5d7f2c80263a19f25
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-28 13:23:07 +0000

    solve: 339B - Xenia and Ringroad

---
+++ 2021/2021-12-28/339B_Xenia_and_Ringroad.cpp
@@ -0,0 +1,5 @@
+// Problem: 339B - Xenia and Ringroad
+// URL: https://codeforces.com/problemset/problem/339/B
+// Date: 2021-12-28
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/339/submission/140900498
