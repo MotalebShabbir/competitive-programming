095c677378aa022bf237b0534f6903e26f239126
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-28 08:36:21 +0000

    solve: 149A - Business trip

---
+++ 2021/2021-12-28/149A_Business_trip.cpp
@@ -0,0 +1,5 @@
+// Problem: 149A - Business trip
+// URL: https://codeforces.com/problemset/problem/149/A
+// Date: 2021-12-28
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/149/submission/140873447
