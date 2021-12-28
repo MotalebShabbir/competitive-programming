9853e95044f4fe7078180f2273e50da7a4b315c8
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-28 04:58:34 +0000

    solve: 200B - Drinks

---
+++ 2021/2021-12-28/200B_Drinks.cpp
@@ -0,0 +1,5 @@
+// Problem: 200B - Drinks
+// URL: https://codeforces.com/problemset/problem/200/B
+// Date: 2021-12-28
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/200/submission/140857150
