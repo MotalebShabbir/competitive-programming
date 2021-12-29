379be7b4fba4f6d4714b15ebc5acce4a383a8913
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-29 10:14:00 +0000

    solve: 1339A - Filling Diamonds

---
+++ 2021/2021-12-29/1339A_Filling_Diamonds.cpp
@@ -0,0 +1,5 @@
+// Problem: 1339A - Filling Diamonds
+// URL: https://codeforces.com/problemset/problem/1339/A
+// Date: 2021-12-29
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1339/submission/141010938
