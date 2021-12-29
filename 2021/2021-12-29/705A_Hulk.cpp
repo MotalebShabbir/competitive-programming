317c07e85685902d484ca85f0a5bd4c39d8f8df2
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-29 09:17:12 +0000

    solve: 705A - Hulk

---
+++ 2021/2021-12-29/705A_Hulk.cpp
@@ -0,0 +1,5 @@
+// Problem: 705A - Hulk
+// URL: https://codeforces.com/problemset/problem/705/A
+// Date: 2021-12-29
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/705/submission/141005896
