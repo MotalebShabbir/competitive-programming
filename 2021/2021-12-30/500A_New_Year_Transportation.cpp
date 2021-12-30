7771c088381e0619757d3330d8fe61a8d71ee038
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-30 12:56:49 +0000

    solve: 500A - New Year Transportation

---
+++ 2021/2021-12-30/500A_New_Year_Transportation.cpp
@@ -0,0 +1,5 @@
+// Problem: 500A - New Year Transportation
+// URL: https://codeforces.com/problemset/problem/500/A
+// Date: 2021-12-30
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/500/submission/141162006
