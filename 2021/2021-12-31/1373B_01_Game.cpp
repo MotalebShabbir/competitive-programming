7973aaeeeae3a86c8b72f2e108143ffb571264a8
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-31 08:17:45 +0000

    solve: 1373B - 01 Game

---
+++ 2021/2021-12-31/1373B_01_Game.cpp
@@ -0,0 +1,5 @@
+// Problem: 1373B - 01 Game
+// URL: https://codeforces.com/problemset/problem/1373/B
+// Date: 2021-12-31
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1373/submission/141224379
