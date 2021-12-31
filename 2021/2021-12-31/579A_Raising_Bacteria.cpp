a2199706f1e20af80b4ff7495a8d6880e8fe1624
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-31 03:45:44 +0000

    solve: 579A - Raising Bacteria

---
+++ 2021/2021-12-31/579A_Raising_Bacteria.cpp
@@ -0,0 +1,5 @@
+// Problem: 579A - Raising Bacteria
+// URL: https://codeforces.com/problemset/problem/579/A
+// Date: 2021-12-31
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/579/submission/141209663
