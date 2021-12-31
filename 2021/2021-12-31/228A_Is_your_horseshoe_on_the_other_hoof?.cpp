f52d1c053236b8f14d8f57008642376fb62e6ee3
MotalebShabbir <motalebshabbir@gmail.com>
2021-12-31 02:24:19 +0000

    solve: 228A - Is your horseshoe on the other hoof?

---
+++ 2021/2021-12-31/228A_Is_your_horseshoe_on_the_other_hoof?.cpp
@@ -0,0 +1,5 @@
+// Problem: 228A - Is your horseshoe on the other hoof?
+// URL: https://codeforces.com/problemset/problem/228/A
+// Date: 2021-12-31
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/228/submission/141207208
