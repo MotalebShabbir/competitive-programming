0d0e8fc9842d0490c2ee5b239b629d1b33c4b928
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-01 13:09:51 +0000

    solve: 1341A - Nastya and Rice

---
+++ 2022/2022-01-01/1341A_Nastya_and_Rice.cpp
@@ -0,0 +1,5 @@
+// Problem: 1341A - Nastya and Rice
+// URL: https://codeforces.com/problemset/problem/1341/A
+// Date: 2022-01-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1341/submission/141316294
