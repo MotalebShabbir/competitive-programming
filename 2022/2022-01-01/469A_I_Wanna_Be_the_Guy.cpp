3d7dcd15f3e23410694f960ce7a2ad122eb3852e
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-01 12:20:07 +0000

    solve: 469A - I Wanna Be the Guy

---
+++ 2022/2022-01-01/469A_I_Wanna_Be_the_Guy.cpp
@@ -0,0 +1,5 @@
+// Problem: 469A - I Wanna Be the Guy
+// URL: https://codeforces.com/problemset/problem/469/A
+// Date: 2022-01-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/469/submission/141312468
