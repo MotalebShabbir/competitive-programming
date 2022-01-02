3c97772b8037ab013c3e753351bb2910ccf5db07
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-02 06:57:41 +0000

    solve: 379A - New Year Candles

---
+++ 2022/2022-01-02/379A_New_Year_Candles.cpp
@@ -0,0 +1,5 @@
+// Problem: 379A - New Year Candles
+// URL: https://codeforces.com/problemset/problem/379/A
+// Date: 2022-01-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/379/submission/141371373
