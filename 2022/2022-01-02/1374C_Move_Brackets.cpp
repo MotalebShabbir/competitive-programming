8c08dada3c9e4bc90e0bbfe05593b4ebe572471c
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-02 12:32:20 +0000

    solve: 1374C - Move Brackets

---
+++ 2022/2022-01-02/1374C_Move_Brackets.cpp
@@ -0,0 +1,5 @@
+// Problem: 1374C - Move Brackets
+// URL: https://codeforces.com/problemset/problem/1374/C
+// Date: 2022-01-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1374/submission/141397469
