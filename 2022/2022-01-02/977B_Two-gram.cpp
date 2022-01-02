67b9c036639f14cd93a60cda3a274469b549a9ed
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-02 05:17:19 +0000

    solve: 977B - Two-gram

---
+++ 2022/2022-01-02/977B_Two-gram.cpp
@@ -0,0 +1,5 @@
+// Problem: 977B - Two-gram
+// URL: https://codeforces.com/problemset/problem/977/B
+// Date: 2022-01-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/977/submission/141364664
