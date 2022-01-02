a76db830c56004c68a0f20087aa85b7e82c88a5f
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-02 01:38:22 +0000

    solve: 148A - Insomnia cure

---
+++ 2022/2022-01-02/148A_Insomnia_cure.cpp
@@ -0,0 +1,5 @@
+// Problem: 148A - Insomnia cure
+// URL: https://codeforces.com/problemset/problem/148/A
+// Date: 2022-01-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/148/submission/141357097
