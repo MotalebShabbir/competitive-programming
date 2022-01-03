daedb60cb56d2ec53f1a82541671241abeca9004
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-03 01:32:24 +0000

    solve: 144A - Arrival of the General

---
+++ 2022/2022-01-03/144A_Arrival_of_the_General.cpp
@@ -0,0 +1,5 @@
+// Problem: 144A - Arrival of the General
+// URL: https://codeforces.com/problemset/problem/144/A
+// Date: 2022-01-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/144/submission/141443801
