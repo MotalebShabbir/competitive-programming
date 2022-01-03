ac7beef4cf357a80799b393d3682e41546148de4
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-03 02:22:58 +0000

    solve: 1296B - Food Buying

---
+++ 2022/2022-01-03/1296B_Food_Buying.cpp
@@ -0,0 +1,5 @@
+// Problem: 1296B - Food Buying
+// URL: https://codeforces.com/problemset/problem/1296/B
+// Date: 2022-01-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1296/submission/141445027
