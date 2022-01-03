1e99cd64e8ba068c3e132951f77c70c65fd73ff0
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-03 06:25:07 +0000

    solve: 584A - Olesya and Rodion

---
+++ 2022/2022-01-03/584A_Olesya_and_Rodion.cpp
@@ -0,0 +1,5 @@
+// Problem: 584A - Olesya and Rodion
+// URL: https://codeforces.com/problemset/problem/584/A
+// Date: 2022-01-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/584/submission/141456232
