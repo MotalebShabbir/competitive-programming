8d3da906a0110cea2e10fd189948441629be7f61
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-04 01:38:23 +0000

    solve: 1328A - Divisibility Problem

---
+++ 2022/2022-01-04/1328A_Divisibility_Problem.cpp
@@ -0,0 +1,5 @@
+// Problem: 1328A - Divisibility Problem
+// URL: https://codeforces.com/problemset/problem/1328/A
+// Date: 2022-01-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1328/submission/141594242
