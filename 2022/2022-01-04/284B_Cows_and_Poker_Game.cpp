ea619db4d93e27fee53454e7bf71201c1d9c1e2a
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-04 09:45:59 +0000

    solve: 284B - Cows and Poker Game

---
+++ 2022/2022-01-04/284B_Cows_and_Poker_Game.cpp
@@ -0,0 +1,5 @@
+// Problem: 284B - Cows and Poker Game
+// URL: https://codeforces.com/problemset/problem/284/B
+// Date: 2022-01-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/284/submission/141626506
