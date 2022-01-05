4bb9c74df0aa7d97aaf0be6fc6904887f9aae129
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-05 09:25:52 +0000

    solve: 1613A - Long Comparison

---
+++ 2022/2022-01-05/1613A_Long_Comparison.cpp
@@ -0,0 +1,5 @@
+// Problem: 1613A - Long Comparison
+// URL: https://codeforces.com/problemset/problem/1613/A
+// Date: 2022-01-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1613/submission/141716202
