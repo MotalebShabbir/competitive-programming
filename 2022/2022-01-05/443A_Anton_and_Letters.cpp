91672d7caf26b7f7a723e8e05a9c5cffbea56ca7
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-05 04:46:16 +0000

    solve: 443A - Anton and Letters

---
+++ 2022/2022-01-05/443A_Anton_and_Letters.cpp
@@ -0,0 +1,5 @@
+// Problem: 443A - Anton and Letters
+// URL: https://codeforces.com/problemset/problem/443/A
+// Date: 2022-01-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/443/submission/141695613
