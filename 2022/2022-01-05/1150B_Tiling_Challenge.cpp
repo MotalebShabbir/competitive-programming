12ac7f636ab5d31096df57ae6d7c6c3651c0ca67
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-05 03:01:38 +0000

    solve: 1150B - Tiling Challenge

---
+++ 2022/2022-01-05/1150B_Tiling_Challenge.cpp
@@ -0,0 +1,5 @@
+// Problem: 1150B - Tiling Challenge
+// URL: https://codeforces.com/problemset/problem/1150/B
+// Date: 2022-01-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1150/submission/141691357
