5c31439c614c5ce2cffa565f6de7ba34bc1f27a6
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-06 11:03:29 +0000

    solve: 520A - Pangram

---
+++ 2022/2022-01-06/520A_Pangram.cpp
@@ -0,0 +1,5 @@
+// Problem: 520A - Pangram
+// URL: https://codeforces.com/problemset/problem/520/A
+// Date: 2022-01-06
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/520/submission/141809962
