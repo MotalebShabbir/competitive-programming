dfd134c1ab373d25879f5a3ef8b2d895df0f49ff
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-07 05:31:16 +0000

    solve: 268A - Games

---
+++ 2022/2022-01-07/268A_Games.cpp
@@ -0,0 +1,5 @@
+// Problem: 268A - Games
+// URL: https://codeforces.com/problemset/problem/268/A
+// Date: 2022-01-07
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/268/submission/141875542
