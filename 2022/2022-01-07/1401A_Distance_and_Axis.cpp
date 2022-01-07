76c6cca22300e5f73f0eee17426432d4e635161a
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-07 12:23:47 +0000

    solve: 1401A - Distance and Axis

---
+++ 2022/2022-01-07/1401A_Distance_and_Axis.cpp
@@ -0,0 +1,5 @@
+// Problem: 1401A - Distance and Axis
+// URL: https://codeforces.com/problemset/problem/1401/A
+// Date: 2022-01-07
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1401/submission/141906254
