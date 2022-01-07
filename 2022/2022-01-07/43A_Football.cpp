0085424759ee979db8f53580a163c3dbcdaf80c7
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-07 14:50:41 +0000

    solve: 43A - Football

---
+++ 2022/2022-01-07/43A_Football.cpp
@@ -0,0 +1,5 @@
+// Problem: 43A - Football
+// URL: https://codeforces.com/problemset/problem/43/A
+// Date: 2022-01-07
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/43/submission/141920046
