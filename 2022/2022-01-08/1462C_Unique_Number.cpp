2281238de7d40f14c24486cbb7503eae3a8f8895
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-08 02:42:40 +0000

    solve: 1462C - Unique Number

---
+++ 2022/2022-01-08/1462C_Unique_Number.cpp
@@ -0,0 +1,5 @@
+// Problem: 1462C - Unique Number
+// URL: https://codeforces.com/problemset/problem/1462/C
+// Date: 2022-01-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1462/submission/141956365
