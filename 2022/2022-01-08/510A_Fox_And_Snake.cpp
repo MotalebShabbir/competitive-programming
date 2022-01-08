e9df5e895859e8196bc859f2bb1cd00ac7537901
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-08 02:15:33 +0000

    solve: 510A - Fox And Snake

---
+++ 2022/2022-01-08/510A_Fox_And_Snake.cpp
@@ -0,0 +1,5 @@
+// Problem: 510A - Fox And Snake
+// URL: https://codeforces.com/problemset/problem/510/A
+// Date: 2022-01-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/510/submission/141955529
