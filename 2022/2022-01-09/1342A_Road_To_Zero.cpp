7d2946e4464b38007277840f09557637418f2e7e
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-09 12:57:45 +0000

    solve: 1342A - Road To Zero

---
+++ 2022/2022-01-09/1342A_Road_To_Zero.cpp
@@ -0,0 +1,5 @@
+// Problem: 1342A - Road To Zero
+// URL: https://codeforces.com/problemset/problem/1342/A
+// Date: 2022-01-09
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1342/submission/142085099
