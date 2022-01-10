abcf87182bc84f17fa88641c1b509f91842124c9
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-10 12:24:25 +0000

    solve: 996A - Hit the Lottery

---
+++ 2022/2022-01-10/996A_Hit_the_Lottery.cpp
@@ -0,0 +1,5 @@
+// Problem: 996A - Hit the Lottery
+// URL: https://codeforces.com/problemset/problem/996/A
+// Date: 2022-01-10
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/996/submission/142176020
