c2af8dc1433e8dd71bd43070299dba3bf584d161
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-11 03:31:35 +0000

    solve: 1350A - Orac and Factors

---
+++ 2022/2022-01-11/1350A_Orac_and_Factors.cpp
@@ -0,0 +1,5 @@
+// Problem: 1350A - Orac and Factors
+// URL: https://codeforces.com/problemset/problem/1350/A
+// Date: 2022-01-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1350/submission/142320171
