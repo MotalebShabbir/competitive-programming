0364284b6cb5c055a544843cbb077a074eb97a6e
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-11 10:58:37 +0000

    solve: 577A - Multiplication Table

---
+++ 2022/2022-01-11/577A_Multiplication_Table.cpp
@@ -0,0 +1,5 @@
+// Problem: 577A - Multiplication Table
+// URL: https://codeforces.com/problemset/problem/577/A
+// Date: 2022-01-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/577/submission/142359578
