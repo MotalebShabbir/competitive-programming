b7580ee92676909928a5e8a2cd54a980af5c85c2
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-13 01:21:03 +0000

    solve: 141A - Amusing Joke

---
+++ 2022/2022-01-13/141A_Amusing_Joke.cpp
@@ -0,0 +1,5 @@
+// Problem: 141A - Amusing Joke
+// URL: https://codeforces.com/problemset/problem/141/A
+// Date: 2022-01-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/141/submission/142555236
