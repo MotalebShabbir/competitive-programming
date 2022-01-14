1d17e8057db3ea4f01733456f7db70c142978644
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-14 05:03:33 +0000

    solve: 785A - Anton and Polyhedrons

---
+++ 2022/2022-01-14/785A_Anton_and_Polyhedrons.cpp
@@ -0,0 +1,5 @@
+// Problem: 785A - Anton and Polyhedrons
+// URL: https://codeforces.com/problemset/problem/785/A
+// Date: 2022-01-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/785/submission/142661216
