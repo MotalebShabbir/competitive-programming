7c51323a150d7cdabb11774f05f761fa8f023b60
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-14 04:47:27 +0000

    solve: 476A - Dreamoon and Stairs

---
+++ 2022/2022-01-14/476A_Dreamoon_and_Stairs.cpp
@@ -0,0 +1,5 @@
+// Problem: 476A - Dreamoon and Stairs
+// URL: https://codeforces.com/problemset/problem/476/A
+// Date: 2022-01-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/476/submission/142660365
