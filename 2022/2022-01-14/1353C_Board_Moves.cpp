13f19ae203977f59853b020114097aa280f30fcf
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-14 09:53:20 +0000

    solve: 1353C - Board Moves

---
+++ 2022/2022-01-14/1353C_Board_Moves.cpp
@@ -0,0 +1,5 @@
+// Problem: 1353C - Board Moves
+// URL: https://codeforces.com/problemset/problem/1353/C
+// Date: 2022-01-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1353/submission/142686100
