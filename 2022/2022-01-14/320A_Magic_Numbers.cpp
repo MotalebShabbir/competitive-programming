3a9205fb36c9f65050d5cd9e09fd182c47d9379f
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-14 05:28:23 +0000

    solve: 320A - Magic Numbers

---
+++ 2022/2022-01-14/320A_Magic_Numbers.cpp
@@ -0,0 +1,5 @@
+// Problem: 320A - Magic Numbers
+// URL: https://codeforces.com/problemset/problem/320/A
+// Date: 2022-01-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/320/submission/142662714
