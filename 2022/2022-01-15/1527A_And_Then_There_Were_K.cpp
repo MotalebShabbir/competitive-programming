a70b5b3da2b6c3d85d07f8f3997847d2f93596d2
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-15 02:47:54 +0000

    solve: 1527A - And Then There Were K

---
+++ 2022/2022-01-15/1527A_And_Then_There_Were_K.cpp
@@ -0,0 +1,5 @@
+// Problem: 1527A - And Then There Were K
+// URL: https://codeforces.com/problemset/problem/1527/A
+// Date: 2022-01-15
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1527/submission/142749915
