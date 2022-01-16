5a41144288e9461632636967841781255366faa0
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-16 04:23:24 +0000

    solve: 1324A - Yet Another Tetris Problem

---
+++ 2022/2022-01-16/1324A_Yet_Another_Tetris_Problem.cpp
@@ -0,0 +1,5 @@
+// Problem: 1324A - Yet Another Tetris Problem
+// URL: https://codeforces.com/problemset/problem/1324/A
+// Date: 2022-01-16
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1324/submission/142911809
