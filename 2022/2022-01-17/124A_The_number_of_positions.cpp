7f276116dfabcb844706020d2ba0d7f62618e456
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-17 14:08:27 +0000

    solve: 124A - The number of positions

---
+++ 2022/2022-01-17/124A_The_number_of_positions.cpp
@@ -0,0 +1,5 @@
+// Problem: 124A - The number of positions
+// URL: https://codeforces.com/problemset/problem/124/A
+// Date: 2022-01-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/124/submission/143129882
