875c1d722300f792c8d840b2aa2fe4c398c29ba6
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-17 11:30:46 +0000

    solve: 1354A - Alarm Clock

---
+++ 2022/2022-01-17/1354A_Alarm_Clock.cpp
@@ -0,0 +1,5 @@
+// Problem: 1354A - Alarm Clock
+// URL: https://codeforces.com/problemset/problem/1354/A
+// Date: 2022-01-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1354/submission/143113370
