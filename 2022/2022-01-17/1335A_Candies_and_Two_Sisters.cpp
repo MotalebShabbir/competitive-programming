f6b6434b295bd8738365de4d68785aec564056d0
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-17 01:17:16 +0000

    solve: 1335A - Candies and Two Sisters

---
+++ 2022/2022-01-17/1335A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,5 @@
+// Problem: 1335A - Candies and Two Sisters
+// URL: https://codeforces.com/problemset/problem/1335/A
+// Date: 2022-01-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1335/submission/143045619
