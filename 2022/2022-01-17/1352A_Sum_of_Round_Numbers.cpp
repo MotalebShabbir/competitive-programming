294f0bf688afc06503a4b29a48056fb79fe082e7
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-17 15:43:01 +0000

    solve: 1352A - Sum of Round Numbers

---
+++ 2022/2022-01-17/1352A_Sum_of_Round_Numbers.cpp
@@ -0,0 +1,5 @@
+// Problem: 1352A - Sum of Round Numbers
+// URL: https://codeforces.com/problemset/problem/1352/A
+// Date: 2022-01-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1352/submission/143140276
