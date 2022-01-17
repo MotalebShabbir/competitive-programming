80b7a4cdc1c02b4196fb82f5d703b606d4b88b69
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-17 02:22:15 +0000

    solve: 567A - Lineland Mail

---
+++ 2022/2022-01-17/567A_Lineland_Mail.cpp
@@ -0,0 +1,5 @@
+// Problem: 567A - Lineland Mail
+// URL: https://codeforces.com/problemset/problem/567/A
+// Date: 2022-01-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/567/submission/143048135
