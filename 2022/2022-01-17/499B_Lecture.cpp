fa494b29dbbbf4fc13846bdd52ee434b5c0f63de
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-17 01:03:31 +0000

    solve: 499B - Lecture

---
+++ 2022/2022-01-17/499B_Lecture.cpp
@@ -0,0 +1,5 @@
+// Problem: 499B - Lecture
+// URL: https://codeforces.com/problemset/problem/499/B
+// Date: 2022-01-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/499/submission/143045261
