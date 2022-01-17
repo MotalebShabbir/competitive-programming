e1c5dd3387ce2f106ed195250842372ea13f2b1e
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-17 02:48:07 +0000

    solve: 742A - Arpa’s hard exam and Mehrdad’s naive cheat

---
+++ 2022/2022-01-17/742A_Arpa’s_hard_exam_and_Mehrdad’s_naive_cheat.cpp
@@ -0,0 +1,5 @@
+// Problem: 742A - Arpa’s hard exam and Mehrdad’s naive cheat
+// URL: https://codeforces.com/problemset/problem/742/A
+// Date: 2022-01-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/742/submission/143049445
