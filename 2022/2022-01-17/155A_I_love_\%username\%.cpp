46ef071ac817d83e303784adeb4b6cdcb9fa7451
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-17 05:18:29 +0000

    solve: 155A - I_love_\%username\%

---
+++ 2022/2022-01-17/155A_I_love_\%username\%.cpp
@@ -0,0 +1,5 @@
+// Problem: 155A - I_love_\%username\%
+// URL: https://codeforces.com/problemset/problem/155/A
+// Date: 2022-01-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/155/submission/143061380
