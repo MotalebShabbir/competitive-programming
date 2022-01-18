4d9145d4d6ea46d0602402a225df03cc7288b230
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-18 06:51:03 +0000

    solve: 1359A - Berland Poker

---
+++ 2022/2022-01-18/1359A_Berland_Poker.cpp
@@ -0,0 +1,5 @@
+// Problem: 1359A - Berland Poker
+// URL: https://codeforces.com/problemset/problem/1359/A
+// Date: 2022-01-18
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1359/submission/143190771
