d23b39b0f1e1299e71ed8a33149aea592459c4ad
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-18 01:50:24 +0000

    solve: 1363A - Odd Selection

---
+++ 2022/2022-01-18/1363A_Odd_Selection.cpp
@@ -0,0 +1,5 @@
+// Problem: 1363A - Odd Selection
+// URL: https://codeforces.com/problemset/problem/1363/A
+// Date: 2022-01-18
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1363/submission/143169168
