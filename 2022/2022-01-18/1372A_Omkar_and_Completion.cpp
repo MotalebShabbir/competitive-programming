853ede2157a01b418aa945fb14af7de1a10303a5
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-18 13:05:39 +0000

    solve: 1372A - Omkar and Completion

---
+++ 2022/2022-01-18/1372A_Omkar_and_Completion.cpp
@@ -0,0 +1,5 @@
+// Problem: 1372A - Omkar and Completion
+// URL: https://codeforces.com/problemset/problem/1372/A
+// Date: 2022-01-18
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1372/submission/143225167
