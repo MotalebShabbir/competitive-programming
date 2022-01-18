4603c27b1673d98dd1388da0dd6d4cc3227b0550
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-18 08:17:49 +0000

    solve: 1370A - Maximum GCD

---
+++ 2022/2022-01-18/1370A_Maximum_GCD.cpp
@@ -0,0 +1,5 @@
+// Problem: 1370A - Maximum GCD
+// URL: https://codeforces.com/problemset/problem/1370/A
+// Date: 2022-01-18
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1370/submission/143198841
