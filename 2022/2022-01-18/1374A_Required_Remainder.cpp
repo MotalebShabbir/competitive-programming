d42a1455620b3a41e005feb7fa20e0572ac42f57
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-18 08:35:00 +0000

    solve: 1374A - Required Remainder

---
+++ 2022/2022-01-18/1374A_Required_Remainder.cpp
@@ -0,0 +1,5 @@
+// Problem: 1374A - Required Remainder
+// URL: https://codeforces.com/problemset/problem/1374/A
+// Date: 2022-01-18
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1374/submission/143200385
