51a877804cba0d6b7ca0b959e7e1d9f359de970b
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-19 16:25:33 +0000

    solve: 1343A - Candies

---
+++ 2022/2022-01-19/1343A_Candies.cpp
@@ -0,0 +1,5 @@
+// Problem: 1343A - Candies
+// URL: https://codeforces.com/problemset/problem/1343/A
+// Date: 2022-01-19
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1343/submission/143347795
