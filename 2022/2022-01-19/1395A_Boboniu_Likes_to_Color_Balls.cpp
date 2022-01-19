aa7ff45d88c158072d88af5c4414bae340991258
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-19 08:58:39 +0000

    solve: 1395A - Boboniu Likes to Color Balls

---
+++ 2022/2022-01-19/1395A_Boboniu_Likes_to_Color_Balls.cpp
@@ -0,0 +1,5 @@
+// Problem: 1395A - Boboniu Likes to Color Balls
+// URL: https://codeforces.com/problemset/problem/1395/A
+// Date: 2022-01-19
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1395/submission/143304984
