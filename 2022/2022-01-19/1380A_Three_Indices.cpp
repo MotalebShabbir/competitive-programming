5738628eabd78c7d89e8674ba78a17adc00a5c68
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-19 02:37:57 +0000

    solve: 1380A - Three Indices

---
+++ 2022/2022-01-19/1380A_Three_Indices.cpp
@@ -0,0 +1,5 @@
+// Problem: 1380A - Three Indices
+// URL: https://codeforces.com/problemset/problem/1380/A
+// Date: 2022-01-19
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1380/submission/143275528
