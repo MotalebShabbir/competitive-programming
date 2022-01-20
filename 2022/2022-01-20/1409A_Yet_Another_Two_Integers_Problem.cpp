45af96e9635d0b980cb26f2de341b1166d0dd087
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-20 15:35:17 +0000

    solve: 1409A - Yet Another Two Integers Problem

---
+++ 2022/2022-01-20/1409A_Yet_Another_Two_Integers_Problem.cpp
@@ -0,0 +1,5 @@
+// Problem: 1409A - Yet Another Two Integers Problem
+// URL: https://codeforces.com/problemset/problem/1409/A
+// Date: 2022-01-20
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1409/submission/143438782
