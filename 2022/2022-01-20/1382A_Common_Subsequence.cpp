b3b369b13828055c3a744a1b7d8297745634cd0a
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-20 13:24:02 +0000

    solve: 1382A - Common Subsequence

---
+++ 2022/2022-01-20/1382A_Common_Subsequence.cpp
@@ -0,0 +1,5 @@
+// Problem: 1382A - Common Subsequence
+// URL: https://codeforces.com/problemset/problem/1382/A
+// Date: 2022-01-20
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1382/submission/143425030
