a375073393bde5728e6615a6c848115a946cd53e
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-20 13:53:50 +0000

    solve: 1405A - Permutation Forgery

---
+++ 2022/2022-01-20/1405A_Permutation_Forgery.cpp
@@ -0,0 +1,5 @@
+// Problem: 1405A - Permutation Forgery
+// URL: https://codeforces.com/problemset/problem/1405/A
+// Date: 2022-01-20
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1405/submission/143428127
