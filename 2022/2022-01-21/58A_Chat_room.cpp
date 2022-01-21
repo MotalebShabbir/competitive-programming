f3766a8ce06bffdffb9e55a43f5b3bad0dadd450
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-21 16:45:23 +0000

    solve: 58A - Chat room

---
+++ 2022/2022-01-21/58A_Chat_room.cpp
@@ -0,0 +1,5 @@
+// Problem: 58A - Chat room
+// URL: https://codeforces.com/problemset/problem/58/A
+// Date: 2022-01-21
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/58/submission/143541898
