0bf919e7158eeb8afd5b71438c05adc3ca3a3474
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-23 02:42:19 +0000

    solve: 1612B - Special Permutation

---
+++ 2022/2022-01-23/1612B_Special_Permutation.cpp
@@ -0,0 +1,5 @@
+// Problem: 1612B - Special Permutation
+// URL: https://codeforces.com/problemset/problem/1612/B
+// Date: 2022-01-23
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1612/submission/143728185
