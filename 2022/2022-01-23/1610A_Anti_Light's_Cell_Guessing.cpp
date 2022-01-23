8cd9419a00fdd521a5ae8031f2eaab74071de50c
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-23 09:47:45 +0000

    solve: 1610A - Anti Light's Cell Guessing

---
+++ 2022/2022-01-23/1610A_Anti_Light's_Cell_Guessing.cpp
@@ -0,0 +1,5 @@
+// Problem: 1610A - Anti Light's Cell Guessing
+// URL: https://codeforces.com/problemset/problem/1610/A
+// Date: 2022-01-23
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1610/submission/143765492
