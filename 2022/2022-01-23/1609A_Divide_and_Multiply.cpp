b01b4bbc90e460b0c4ae94ec188d9bbd00d24a1a
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-23 12:39:28 +0000

    solve: 1609A - Divide and Multiply

---
+++ 2022/2022-01-23/1609A_Divide_and_Multiply.cpp
@@ -0,0 +1,5 @@
+// Problem: 1609A - Divide and Multiply
+// URL: https://codeforces.com/problemset/problem/1609/A
+// Date: 2022-01-23
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1609/submission/143784497
