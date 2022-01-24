5d5bd2ccac9e178c183441f03efed0c2c00666bc
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-24 06:06:55 +0000

    solve: 1326A - Bad Ugly Numbers

---
+++ 2022/2022-01-24/1326A_Bad_Ugly_Numbers.cpp
@@ -0,0 +1,5 @@
+// Problem: 1326A - Bad Ugly Numbers
+// URL: https://codeforces.com/problemset/problem/1326/A
+// Date: 2022-01-24
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1326/submission/143839563
