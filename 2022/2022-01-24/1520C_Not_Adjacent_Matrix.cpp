cc8ec6265efed696d28ce83817a8db5c2e349798
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-24 06:59:44 +0000

    solve: 1520C - Not Adjacent Matrix

---
+++ 2022/2022-01-24/1520C_Not_Adjacent_Matrix.cpp
@@ -0,0 +1,5 @@
+// Problem: 1520C - Not Adjacent Matrix
+// URL: https://codeforces.com/problemset/problem/1520/C
+// Date: 2022-01-24
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1520/submission/143844064
