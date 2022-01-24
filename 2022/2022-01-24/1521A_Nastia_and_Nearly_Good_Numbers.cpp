8ed9606d79df1436db0452c115f06160583adfbd
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-24 01:58:23 +0000

    solve: 1521A - Nastia and Nearly Good Numbers

---
+++ 2022/2022-01-24/1521A_Nastia_and_Nearly_Good_Numbers.cpp
@@ -0,0 +1,5 @@
+// Problem: 1521A - Nastia and Nearly Good Numbers
+// URL: https://codeforces.com/problemset/problem/1521/A
+// Date: 2022-01-24
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1521/submission/143826659
