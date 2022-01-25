5fb001f496ab339a53a8dda32ae658243604d925
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-25 11:22:37 +0000

    solve: 1355A - Sequence with Digits

---
+++ 2022/2022-01-25/1355A_Sequence_with_Digits.cpp
@@ -0,0 +1,5 @@
+// Problem: 1355A - Sequence with Digits
+// URL: https://codeforces.com/problemset/problem/1355/A
+// Date: 2022-01-25
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1355/submission/143965570
