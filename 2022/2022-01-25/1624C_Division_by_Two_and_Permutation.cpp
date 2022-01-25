1458b53592d9513993dd36bcda6002efa547d665
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-25 09:00:16 +0000

    solve: 1624C - Division by Two and Permutation

---
+++ 2022/2022-01-25/1624C_Division_by_Two_and_Permutation.cpp
@@ -0,0 +1,5 @@
+// Problem: 1624C - Division by Two and Permutation
+// URL: https://codeforces.com/problemset/problem/1624/C
+// Date: 2022-01-25
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1624/submission/143952683
