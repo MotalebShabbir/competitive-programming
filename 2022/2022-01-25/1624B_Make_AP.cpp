8ce9111bdafd07d55a0e8940e2ae0c2b8f5fb3d6
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-25 06:47:34 +0000

    solve: 1624B - Make AP

---
+++ 2022/2022-01-25/1624B_Make_AP.cpp
@@ -0,0 +1,5 @@
+// Problem: 1624B - Make AP
+// URL: https://codeforces.com/problemset/problem/1624/B
+// Date: 2022-01-25
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1624/submission/143938915
