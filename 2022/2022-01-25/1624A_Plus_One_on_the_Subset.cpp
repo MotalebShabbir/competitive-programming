e4b4ebe15d937e9da6ad5e323a279360175b648d
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-25 06:42:54 +0000

    solve: 1624A - Plus One on the Subset

---
+++ 2022/2022-01-25/1624A_Plus_One_on_the_Subset.cpp
@@ -0,0 +1,5 @@
+// Problem: 1624A - Plus One on the Subset
+// URL: https://codeforces.com/problemset/problem/1624/A
+// Date: 2022-01-25
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1624/submission/143938434
