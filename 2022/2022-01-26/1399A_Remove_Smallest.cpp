ee3050b68f1fae0bb4d9781d58d318d342176d10
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-26 11:14:49 +0000

    solve: 1399A - Remove Smallest

---
+++ 2022/2022-01-26/1399A_Remove_Smallest.cpp
@@ -0,0 +1,5 @@
+// Problem: 1399A - Remove Smallest
+// URL: https://codeforces.com/problemset/problem/1399/A
+// Date: 2022-01-26
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1399/submission/144061511
