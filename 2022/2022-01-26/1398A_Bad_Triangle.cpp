9b6172c56df92db913f421e25967c9914232ba7f
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-26 15:44:56 +0000

    solve: 1398A - Bad Triangle

---
+++ 2022/2022-01-26/1398A_Bad_Triangle.cpp
@@ -0,0 +1,5 @@
+// Problem: 1398A - Bad Triangle
+// URL: https://codeforces.com/problemset/problem/1398/A
+// Date: 2022-01-26
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1398/submission/144086963
