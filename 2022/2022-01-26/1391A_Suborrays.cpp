4446b857e363556cfe50a68a417a8673066e3ad6
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-26 14:29:06 +0000

    solve: 1391A - Suborrays

---
+++ 2022/2022-01-26/1391A_Suborrays.cpp
@@ -0,0 +1,5 @@
+// Problem: 1391A - Suborrays
+// URL: https://codeforces.com/problemset/problem/1391/A
+// Date: 2022-01-26
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1391/submission/144080241
