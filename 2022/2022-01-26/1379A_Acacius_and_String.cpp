764dd80629982dffd5b2333de596e91e942f6e89
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-26 08:54:02 +0000

    solve: 1379A - Acacius and String

---
+++ 2022/2022-01-26/1379A_Acacius_and_String.cpp
@@ -0,0 +1,5 @@
+// Problem: 1379A - Acacius and String
+// URL: https://codeforces.com/problemset/problem/1379/A
+// Date: 2022-01-26
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1379/submission/144049558
