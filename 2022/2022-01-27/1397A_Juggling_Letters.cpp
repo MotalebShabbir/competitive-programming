87383559b7a323d5506e700be42771229d9efb15
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-27 06:29:25 +0000

    solve: 1397A - Juggling Letters

---
+++ 2022/2022-01-27/1397A_Juggling_Letters.cpp
@@ -0,0 +1,5 @@
+// Problem: 1397A - Juggling Letters
+// URL: https://codeforces.com/problemset/problem/1397/A
+// Date: 2022-01-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1397/submission/144128251
