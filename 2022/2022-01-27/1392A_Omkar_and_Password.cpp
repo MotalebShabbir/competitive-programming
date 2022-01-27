ee3cc93382811e9629b88f1f51041eaa8061e157
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-27 05:18:24 +0000

    solve: 1392A - Omkar and Password

---
+++ 2022/2022-01-27/1392A_Omkar_and_Password.cpp
@@ -0,0 +1,5 @@
+// Problem: 1392A - Omkar and Password
+// URL: https://codeforces.com/problemset/problem/1392/A
+// Date: 2022-01-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1392/submission/144122936
