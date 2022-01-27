7bdfc627e11c04f83cdc44ac80dd1d4410317847
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-27 12:20:06 +0000

    solve: 462B - Appleman and Card Game

---
+++ 2022/2022-01-27/462B_Appleman_and_Card_Game.cpp
@@ -0,0 +1,5 @@
+// Problem: 462B - Appleman and Card Game
+// URL: https://codeforces.com/problemset/problem/462/B
+// Date: 2022-01-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/462/submission/144156937
