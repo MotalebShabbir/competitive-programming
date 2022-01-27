15c9f75afc398d7a14ede4c5f6f84272a368c4a9
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-27 06:00:20 +0000

    solve: 1400A - String Similarity

---
+++ 2022/2022-01-27/1400A_String_Similarity.cpp
@@ -0,0 +1,5 @@
+// Problem: 1400A - String Similarity
+// URL: https://codeforces.com/problemset/problem/1400/A
+// Date: 2022-01-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1400/submission/144125948
