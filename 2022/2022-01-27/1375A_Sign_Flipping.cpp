f848976a4008a217e51b41b165317bb2d8b3840c
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-27 11:22:56 +0000

    solve: 1375A - Sign Flipping

---
+++ 2022/2022-01-27/1375A_Sign_Flipping.cpp
@@ -0,0 +1,5 @@
+// Problem: 1375A - Sign Flipping
+// URL: https://codeforces.com/problemset/problem/1375/A
+// Date: 2022-01-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1375/submission/144152011
