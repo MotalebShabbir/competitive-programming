9a41284c27bdb2212577d68d81ae722d0bb1237a
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-27 09:11:13 +0000

    solve: 1393A - Rainbow Dash, Fluttershy and Chess Coloring

---
+++ 2022/2022-01-27/1393A_Rainbow_Dash,_Fluttershy_and_Chess_Coloring.cpp
@@ -0,0 +1,5 @@
+// Problem: 1393A - Rainbow Dash, Fluttershy and Chess Coloring
+// URL: https://codeforces.com/problemset/problem/1393/A
+// Date: 2022-01-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1393/submission/144141717
