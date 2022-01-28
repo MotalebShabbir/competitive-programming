688f9ce6a979a31373c5d47e66625100a44d3e84
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-28 13:26:59 +0000

    solve: 581A - Vasya the Hipster

---
+++ 2022/2022-01-28/581A_Vasya_the_Hipster.cpp
@@ -0,0 +1,5 @@
+// Problem: 581A - Vasya the Hipster
+// URL: https://codeforces.com/problemset/problem/581/A
+// Date: 2022-01-28
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/581/submission/144325083
