088e6c948b8780c867fa691129e031ae7d9b9058
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-28 09:01:58 +0000

    solve: 1476A - K-divisible Sum

---
+++ 2022/2022-01-28/1476A_K-divisible_Sum.cpp
@@ -0,0 +1,5 @@
+// Problem: 1476A - K-divisible Sum
+// URL: https://codeforces.com/problemset/problem/1476/A
+// Date: 2022-01-28
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1476/submission/144301933
