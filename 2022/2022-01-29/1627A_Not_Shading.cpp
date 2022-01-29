1dddd56a05627e02f66bbe1936ce43475785cb71
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-29 14:46:32 +0000

    solve: 1627A - Not Shading

---
+++ 2022/2022-01-29/1627A_Not_Shading.cpp
@@ -0,0 +1,5 @@
+// Problem: 1627A - Not Shading
+// URL: https://codeforces.com/problemset/problem/1627/A
+// Date: 2022-01-29
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1627/submission/144432341
