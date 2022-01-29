3c47d6ed966814c96d3224b4a5608e137a1d61c7
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-29 04:48:24 +0000

    solve: 588A - Duff and Meat

---
+++ 2022/2022-01-29/588A_Duff_and_Meat.cpp
@@ -0,0 +1,5 @@
+// Problem: 588A - Duff and Meat
+// URL: https://codeforces.com/problemset/problem/588/A
+// Date: 2022-01-29
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/588/submission/144379363
