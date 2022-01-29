413cfcc960dc6963153f4cf68fee92b09fb5ff02
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-29 17:23:47 +0000

    solve: 1622B - Berland Music

---
+++ 2022/2022-01-29/1622B_Berland_Music.cpp
@@ -0,0 +1,5 @@
+// Problem: 1622B - Berland Music
+// URL: https://codeforces.com/problemset/problem/1622/B
+// Date: 2022-01-29
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1622/submission/144446000
