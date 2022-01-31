dd6947fe8bda63bdef754d6ab447bae865be4bc2
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-31 16:10:41 +0000

    solve: 427A - Police Recruits

---
+++ 2022/2022-01-31/427A_Police_Recruits.cpp
@@ -0,0 +1,5 @@
+// Problem: 427A - Police Recruits
+// URL: https://codeforces.com/problemset/problem/427/A
+// Date: 2022-01-31
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/427/submission/144740876
