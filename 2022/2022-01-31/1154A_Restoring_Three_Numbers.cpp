36d9ef2084fbe2f1ffede09d6da496ef85468cd5
MotalebShabbir <motalebshabbir@gmail.com>
2022-01-31 16:34:56 +0000

    solve: 1154A - Restoring Three Numbers

---
+++ 2022/2022-01-31/1154A_Restoring_Three_Numbers.cpp
@@ -0,0 +1,5 @@
+// Problem: 1154A - Restoring Three Numbers
+// URL: https://codeforces.com/problemset/problem/1154/A
+// Date: 2022-01-31
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1154/submission/144754724
