830dab3b24f904fe2e3f8491a808fd47d1c6c7c4
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-01 11:28:56 +0000

    solve: 1607B - Odd Grasshopper

---
+++ 2022/2022-02-01/1607B_Odd_Grasshopper.cpp
@@ -0,0 +1,5 @@
+// Problem: 1607B - Odd Grasshopper
+// URL: https://codeforces.com/problemset/problem/1607/B
+// Date: 2022-02-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1607/submission/144830064
