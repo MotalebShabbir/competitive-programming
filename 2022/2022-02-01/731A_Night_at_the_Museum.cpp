8094f40881055976816e51a871fbe60ae9fbefaa
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-01 08:15:29 +0000

    solve: 731A - Night at the Museum

---
+++ 2022/2022-02-01/731A_Night_at_the_Museum.cpp
@@ -0,0 +1,5 @@
+// Problem: 731A - Night at the Museum
+// URL: https://codeforces.com/problemset/problem/731/A
+// Date: 2022-02-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/731/submission/144812572
