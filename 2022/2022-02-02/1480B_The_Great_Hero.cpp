5e6395c2f20ca03014867a7e81d8e3293335d76e
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-02 08:58:03 +0000

    solve: 1480B - The Great Hero

---
+++ 2022/2022-02-02/1480B_The_Great_Hero.cpp
@@ -0,0 +1,5 @@
+// Problem: 1480B - The Great Hero
+// URL: https://codeforces.com/problemset/problem/1480/B
+// Date: 2022-02-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1480/submission/144917633
