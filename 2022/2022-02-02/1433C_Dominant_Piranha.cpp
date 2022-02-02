965a2594113fb745b478b89e1ec555b514f512f0
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-02 11:53:32 +0000

    solve: 1433C - Dominant Piranha

---
+++ 2022/2022-02-02/1433C_Dominant_Piranha.cpp
@@ -0,0 +1,5 @@
+// Problem: 1433C - Dominant Piranha
+// URL: https://codeforces.com/problemset/problem/1433/C
+// Date: 2022-02-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1433/submission/144933296
