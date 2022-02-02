fd0f6cf46859e57c80410985efb0eee47f821146
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-02 10:04:22 +0000

    solve: 1359B - New Theatre Square

---
+++ 2022/2022-02-02/1359B_New_Theatre_Square.cpp
@@ -0,0 +1,5 @@
+// Problem: 1359B - New Theatre Square
+// URL: https://codeforces.com/problemset/problem/1359/B
+// Date: 2022-02-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1359/submission/144923300
