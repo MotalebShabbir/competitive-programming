374609abcec50863b50a4162c57273c5371a5649
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-03 06:19:12 +0000

    solve: 1358B - Maria Breaks the Self-isolation

---
+++ 2022/2022-02-03/1358B_Maria_Breaks_the_Self-isolation.cpp
@@ -0,0 +1,5 @@
+// Problem: 1358B - Maria Breaks the Self-isolation
+// URL: https://codeforces.com/problemset/problem/1358/B
+// Date: 2022-02-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1358/submission/144997847
