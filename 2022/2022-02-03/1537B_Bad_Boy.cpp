9db24dcac4e6e5c4700a4e069caf02d4dba663f6
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-03 16:08:05 +0000

    solve: 1537B - Bad Boy

---
+++ 2022/2022-02-03/1537B_Bad_Boy.cpp
@@ -0,0 +1,5 @@
+// Problem: 1537B - Bad Boy
+// URL: https://codeforces.com/problemset/problem/1537/B
+// Date: 2022-02-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1537/submission/145053840
