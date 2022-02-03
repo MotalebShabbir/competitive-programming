c610ed4a75068b74fcc2d7853a635f5f1e9e0d58
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-03 02:59:31 +0000

    solve: 1256A - Payment Without Change

---
+++ 2022/2022-02-03/1256A_Payment_Without_Change.cpp
@@ -0,0 +1,5 @@
+// Problem: 1256A - Payment Without Change
+// URL: https://codeforces.com/problemset/problem/1256/A
+// Date: 2022-02-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1256/submission/144987408
