713ff1438a46ee1e9bfb91b3b7a967dec52d581c
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-03 02:25:32 +0000

    solve: 268B - Buttons

---
+++ 2022/2022-02-03/268B_Buttons.cpp
@@ -0,0 +1,5 @@
+// Problem: 268B - Buttons
+// URL: https://codeforces.com/problemset/problem/268/B
+// Date: 2022-02-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/268/submission/144986514
