5e69773baf302340802cbaff18e7745d2183ef93
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-04 02:28:49 +0000

    solve: 1555A - PizzaForces

---
+++ 2022/2022-02-04/1555A_PizzaForces.cpp
@@ -0,0 +1,5 @@
+// Problem: 1555A - PizzaForces
+// URL: https://codeforces.com/problemset/problem/1555/A
+// Date: 2022-02-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1555/submission/145084364
