4dff4355f1ce8597444479ab30b2048d13753d67
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-04 01:48:40 +0000

    solve: 688B - Lovely Palindromes

---
+++ 2022/2022-02-04/688B_Lovely_Palindromes.cpp
@@ -0,0 +1,5 @@
+// Problem: 688B - Lovely Palindromes
+// URL: https://codeforces.com/problemset/problem/688/B
+// Date: 2022-02-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/688/submission/145083384
