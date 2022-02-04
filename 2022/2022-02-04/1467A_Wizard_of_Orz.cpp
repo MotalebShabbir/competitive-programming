b52d296f3177300fd5046ef55dc8353f94903ee0
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-04 13:19:35 +0000

    solve: 1467A - Wizard of Orz

---
+++ 2022/2022-02-04/1467A_Wizard_of_Orz.cpp
@@ -0,0 +1,5 @@
+// Problem: 1467A - Wizard of Orz
+// URL: https://codeforces.com/problemset/problem/1467/A
+// Date: 2022-02-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1467/submission/145130327
