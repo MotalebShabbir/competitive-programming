0c4ae83845282dda320712fde0c52a1dd107423e
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-04 01:34:07 +0000

    solve: 1333A - Little Artem

---
+++ 2022/2022-02-04/1333A_Little_Artem.cpp
@@ -0,0 +1,5 @@
+// Problem: 1333A - Little Artem
+// URL: https://codeforces.com/problemset/problem/1333/A
+// Date: 2022-02-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1333/submission/145083075
