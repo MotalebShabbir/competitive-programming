88e8cd2e23d589012c757dec98f2035a18e95d1f
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-04 16:22:30 +0000

    solve: 750A - New Year and Hurry

---
+++ 2022/2022-02-04/750A_New_Year_and_Hurry.cpp
@@ -0,0 +1,5 @@
+// Problem: 750A - New Year and Hurry
+// URL: https://codeforces.com/problemset/problem/750/A
+// Date: 2022-02-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/750/submission/145150492
