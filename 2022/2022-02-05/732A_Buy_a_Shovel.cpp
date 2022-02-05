c9fbbbce9e24f047c70d01a6841281e9d4031180
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-05 02:11:30 +0000

    solve: 732A - Buy a Shovel

---
+++ 2022/2022-02-05/732A_Buy_a_Shovel.cpp
@@ -0,0 +1,5 @@
+// Problem: 732A - Buy a Shovel
+// URL: https://codeforces.com/problemset/problem/732/A
+// Date: 2022-02-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/732/submission/145180871
