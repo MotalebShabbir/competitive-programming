18cf47ca542a0aad9886cf64f7a51c4ee6575087
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-05 07:07:51 +0000

    solve: 1490B - Balanced Remainders

---
+++ 2022/2022-02-05/1490B_Balanced_Remainders.cpp
@@ -0,0 +1,5 @@
+// Problem: 1490B - Balanced Remainders
+// URL: https://codeforces.com/problemset/problem/1490/B
+// Date: 2022-02-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1490/submission/145194797
