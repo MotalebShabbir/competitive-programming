630dcde68f081fa937b57b645f98129b8dea1200
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-05 04:57:51 +0000

    solve: 1206B - Make Product Equal One

---
+++ 2022/2022-02-05/1206B_Make_Product_Equal_One.cpp
@@ -0,0 +1,5 @@
+// Problem: 1206B - Make Product Equal One
+// URL: https://codeforces.com/problemset/problem/1206/B
+// Date: 2022-02-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1206/submission/145186583
