9996f4a8ee31bd0e92581f07109c575c26cb5500
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-05 12:07:27 +0000

    solve: 1469A - Regular Bracket Sequence

---
+++ 2022/2022-02-05/1469A_Regular_Bracket_Sequence.cpp
@@ -0,0 +1,5 @@
+// Problem: 1469A - Regular Bracket Sequence
+// URL: https://codeforces.com/problemset/problem/1469/A
+// Date: 2022-02-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1469/submission/145253308
