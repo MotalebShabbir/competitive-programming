36e68ddffadd3a77de7cd5a4876553b21d061b8b
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-05 02:34:55 +0000

    solve: 1326B - Maximums

---
+++ 2022/2022-02-05/1326B_Maximums.cpp
@@ -0,0 +1,5 @@
+// Problem: 1326B - Maximums
+// URL: https://codeforces.com/problemset/problem/1326/B
+// Date: 2022-02-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1326/submission/145181450
