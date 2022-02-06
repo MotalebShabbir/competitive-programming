43185aab015df10143737d3ce93f52aa5b3b40e1
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-06 06:27:06 +0000

    solve: 766B - Mahmoud and a Triangle

---
+++ 2022/2022-02-06/766B_Mahmoud_and_a_Triangle.cpp
@@ -0,0 +1,5 @@
+// Problem: 766B - Mahmoud and a Triangle
+// URL: https://codeforces.com/problemset/problem/766/B
+// Date: 2022-02-06
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/766/submission/145349122
