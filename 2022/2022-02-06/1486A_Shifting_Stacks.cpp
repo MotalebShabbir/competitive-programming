bc899608b78f88211057c710a807f95dea31bb50
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-06 09:50:17 +0000

    solve: 1486A - Shifting Stacks

---
+++ 2022/2022-02-06/1486A_Shifting_Stacks.cpp
@@ -0,0 +1,5 @@
+// Problem: 1486A - Shifting Stacks
+// URL: https://codeforces.com/problemset/problem/1486/A
+// Date: 2022-02-06
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1486/submission/145365415
