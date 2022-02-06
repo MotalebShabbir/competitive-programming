071766efe6b0292ad4620aa8dadc6f7a95084641
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-06 10:35:03 +0000

    solve: 450A - Jzzhu and Children

---
+++ 2022/2022-02-06/450A_Jzzhu_and_Children.cpp
@@ -0,0 +1,5 @@
+// Problem: 450A - Jzzhu and Children
+// URL: https://codeforces.com/problemset/problem/450/A
+// Date: 2022-02-06
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/450/submission/145369095
