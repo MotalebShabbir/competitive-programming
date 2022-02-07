2dd666291c4a6ddbd1d3905aacd5b51a3048aeba
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-07 06:23:01 +0000

    solve: 472A - Design Tutorial: Learn from Math

---
+++ 2022/2022-02-07/472A_Design_Tutorial:_Learn_from_Math.cpp
@@ -0,0 +1,5 @@
+// Problem: 472A - Design Tutorial: Learn from Math
+// URL: https://codeforces.com/problemset/problem/472/A
+// Date: 2022-02-07
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/472/submission/145504993
