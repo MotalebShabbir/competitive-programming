f64968985ce08b65a7b3f3dcad532b53601d9cec
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-07 05:50:55 +0000

    solve: 439A - Devu, the Singer and Churu, the Joker

---
+++ 2022/2022-02-07/439A_Devu,_the_Singer_and_Churu,_the_Joker.cpp
@@ -0,0 +1,5 @@
+// Problem: 439A - Devu, the Singer and Churu, the Joker
+// URL: https://codeforces.com/problemset/problem/439/A
+// Date: 2022-02-07
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/439/submission/145502346
