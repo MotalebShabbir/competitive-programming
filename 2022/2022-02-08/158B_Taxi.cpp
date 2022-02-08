3bd6cc8083fc8da8b6e6953f524a642f6fe8f783
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-08 17:02:44 +0000

    solve: 158B - Taxi

---
+++ 2022/2022-02-08/158B_Taxi.cpp
@@ -0,0 +1,5 @@
+// Problem: 158B - Taxi
+// URL: https://codeforces.com/problemset/problem/158/B
+// Date: 2022-02-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/158/submission/145674297
