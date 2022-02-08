ee4380cb9c006d405c007ebf6dbf941116cdda3e
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-08 09:18:24 +0000

    solve: 1367A - Short Substrings

---
+++ 2022/2022-02-08/1367A_Short_Substrings.cpp
@@ -0,0 +1,5 @@
+// Problem: 1367A - Short Substrings
+// URL: https://codeforces.com/problemset/problem/1367/A
+// Date: 2022-02-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1367/submission/145627349
