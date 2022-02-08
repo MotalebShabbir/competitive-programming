31a91af5327b62368e98c0a6520f3b731b8b18a0
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-08 16:31:46 +0000

    solve: 617A - Elephant

---
+++ 2022/2022-02-08/617A_Elephant.cpp
@@ -0,0 +1,5 @@
+// Problem: 617A - Elephant
+// URL: https://codeforces.com/problemset/problem/617/A
+// Date: 2022-02-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/617/submission/145670921
