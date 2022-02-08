a042228b84a1b7bd623c0b85a09ef5ee0dfbb27e
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-08 16:25:33 +0000

    solve: 50A - Domino piling

---
+++ 2022/2022-02-08/50A_Domino_piling.cpp
@@ -0,0 +1,5 @@
+// Problem: 50A - Domino piling
+// URL: https://codeforces.com/problemset/problem/50/A
+// Date: 2022-02-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/50/submission/145670304
