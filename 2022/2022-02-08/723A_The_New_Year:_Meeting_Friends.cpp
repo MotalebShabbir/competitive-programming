3bfbae3753ccdc73623ebeb4a2e22fd4adab08dd
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-08 16:23:34 +0000

    solve: 723A - The New Year: Meeting Friends

---
+++ 2022/2022-02-08/723A_The_New_Year:_Meeting_Friends.cpp
@@ -0,0 +1,5 @@
+// Problem: 723A - The New Year: Meeting Friends
+// URL: https://codeforces.com/problemset/problem/723/A
+// Date: 2022-02-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/723/submission/145670081
