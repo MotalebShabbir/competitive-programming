13e3997c0779c7d60691165a609e634eff9059b9
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-09 01:13:59 +0000

    solve: 1471A - Strange Partition

---
+++ 2022/2022-02-09/1471A_Strange_Partition.cpp
@@ -0,0 +1,5 @@
+// Problem: 1471A - Strange Partition
+// URL: https://codeforces.com/problemset/problem/1471/A
+// Date: 2022-02-09
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1471/submission/145700793
