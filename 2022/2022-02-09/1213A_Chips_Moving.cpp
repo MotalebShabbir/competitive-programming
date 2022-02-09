d5557a38b9e96189a669949f1d75cf20293cadeb
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-09 10:03:12 +0000

    solve: 1213A - Chips Moving

---
+++ 2022/2022-02-09/1213A_Chips_Moving.cpp
@@ -0,0 +1,5 @@
+// Problem: 1213A - Chips Moving
+// URL: https://codeforces.com/problemset/problem/1213/A
+// Date: 2022-02-09
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1213/submission/145735550
