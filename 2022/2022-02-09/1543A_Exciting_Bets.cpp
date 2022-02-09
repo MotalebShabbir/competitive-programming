a5eea4d257e9fe71dbb25cfe66a4046b48b0772b
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-09 05:55:39 +0000

    solve: 1543A - Exciting Bets

---
+++ 2022/2022-02-09/1543A_Exciting_Bets.cpp
@@ -0,0 +1,5 @@
+// Problem: 1543A - Exciting Bets
+// URL: https://codeforces.com/problemset/problem/1543/A
+// Date: 2022-02-09
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1543/submission/145714209
