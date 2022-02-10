c6842568b9cc57dad1b3cefd52599007eeb24cfd
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-10 03:55:38 +0000

    solve: 1373A - Donut Shops

---
+++ 2022/2022-02-10/1373A_Donut_Shops.cpp
@@ -0,0 +1,5 @@
+// Problem: 1373A - Donut Shops
+// URL: https://codeforces.com/problemset/problem/1373/A
+// Date: 2022-02-10
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1373/submission/145808230
