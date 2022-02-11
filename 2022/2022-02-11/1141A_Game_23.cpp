0f3eb8b9cf0fa7a2a5be6222c50cfc322df54141
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-11 08:52:40 +0000

    solve: 1141A - Game 23

---
+++ 2022/2022-02-11/1141A_Game_23.cpp
@@ -0,0 +1,5 @@
+// Problem: 1141A - Game 23
+// URL: https://codeforces.com/problemset/problem/1141/A
+// Date: 2022-02-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1141/submission/145929321
