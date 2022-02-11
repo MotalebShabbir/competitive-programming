5879bd8e8095204f2d8e7b1015a552d2bfc2e243
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-11 17:31:59 +0000

    solve: 1041A - Heist

---
+++ 2022/2022-02-11/1041A_Heist.cpp
@@ -0,0 +1,5 @@
+// Problem: 1041A - Heist
+// URL: https://codeforces.com/problemset/problem/1041/A
+// Date: 2022-02-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1041/submission/145978972
