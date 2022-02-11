6b0b4cc0326451b430ebc524da42bcd38b46db08
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-11 06:33:42 +0000

    solve: 1182A - Filling Shapes

---
+++ 2022/2022-02-11/1182A_Filling_Shapes.cpp
@@ -0,0 +1,5 @@
+// Problem: 1182A - Filling Shapes
+// URL: https://codeforces.com/problemset/problem/1182/A
+// Date: 2022-02-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1182/submission/145918014
