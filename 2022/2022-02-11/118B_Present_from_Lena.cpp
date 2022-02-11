212c22baefe1aa7f10b43b81041230c0342dda02
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-11 03:42:51 +0000

    solve: 118B - Present from Lena

---
+++ 2022/2022-02-11/118B_Present_from_Lena.cpp
@@ -0,0 +1,5 @@
+// Problem: 118B - Present from Lena
+// URL: https://codeforces.com/problemset/problem/118/B
+// Date: 2022-02-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/118/submission/145907192
