e81685d7d250ae7be0a746226997d0fcda323448
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-11 16:58:43 +0000

    solve: 276A - Lunch Rush

---
+++ 2022/2022-02-11/276A_Lunch_Rush.cpp
@@ -0,0 +1,5 @@
+// Problem: 276A - Lunch Rush
+// URL: https://codeforces.com/problemset/problem/276/A
+// Date: 2022-02-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/276/submission/145975118
