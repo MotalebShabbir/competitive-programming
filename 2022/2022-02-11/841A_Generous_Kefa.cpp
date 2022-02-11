164ae73aabadcaf2dc3c6fc1865be980d3fbb36d
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-11 17:45:24 +0000

    solve: 841A - Generous Kefa

---
+++ 2022/2022-02-11/841A_Generous_Kefa.cpp
@@ -0,0 +1,5 @@
+// Problem: 841A - Generous Kefa
+// URL: https://codeforces.com/problemset/problem/841/A
+// Date: 2022-02-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/841/submission/145980409
