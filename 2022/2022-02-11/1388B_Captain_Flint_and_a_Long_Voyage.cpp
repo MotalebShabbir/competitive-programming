3fe578e8c3611070cc7a3169937c91d47486137f
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-11 02:38:40 +0000

    solve: 1388B - Captain Flint and a Long Voyage

---
+++ 2022/2022-02-11/1388B_Captain_Flint_and_a_Long_Voyage.cpp
@@ -0,0 +1,5 @@
+// Problem: 1388B - Captain Flint and a Long Voyage
+// URL: https://codeforces.com/problemset/problem/1388/B
+// Date: 2022-02-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1388/submission/145904254
