957af656eb7e37bdbd233fe00ccca8c642501ec6
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-13 01:04:47 +0000

    solve: 1343B - Balanced Array

---
+++ 2022/2022-02-13/1343B_Balanced_Array.cpp
@@ -0,0 +1,5 @@
+// Problem: 1343B - Balanced Array
+// URL: https://codeforces.com/problemset/problem/1343/B
+// Date: 2022-02-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1343/submission/146183054
