1612d51e9a35f77a79deb5da239caece8c1526cc
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-13 01:50:56 +0000

    solve: 1028B - Unnatural Conditions

---
+++ 2022/2022-02-13/1028B_Unnatural_Conditions.cpp
@@ -0,0 +1,5 @@
+// Problem: 1028B - Unnatural Conditions
+// URL: https://codeforces.com/problemset/problem/1028/B
+// Date: 2022-02-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1028/submission/146184718
