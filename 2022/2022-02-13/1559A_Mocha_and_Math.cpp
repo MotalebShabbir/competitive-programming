49726eea6f4f71520a2e7dac693f2268390eaf31
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-13 03:22:49 +0000

    solve: 1559A - Mocha and Math

---
+++ 2022/2022-02-13/1559A_Mocha_and_Math.cpp
@@ -0,0 +1,5 @@
+// Problem: 1559A - Mocha and Math
+// URL: https://codeforces.com/problemset/problem/1559/A
+// Date: 2022-02-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1559/submission/146188569
