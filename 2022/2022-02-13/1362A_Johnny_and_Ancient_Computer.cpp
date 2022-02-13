7a09d2214a1b927d4da5ca7d98212bac8457b110
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-13 10:25:21 +0000

    solve: 1362A - Johnny and Ancient Computer

---
+++ 2022/2022-02-13/1362A_Johnny_and_Ancient_Computer.cpp
@@ -0,0 +1,5 @@
+// Problem: 1362A - Johnny and Ancient Computer
+// URL: https://codeforces.com/problemset/problem/1362/A
+// Date: 2022-02-13
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1362/submission/146223174
