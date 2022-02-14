1e8f90607d79b480359370798b23088edb70f414
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-14 02:40:48 +0000

    solve: 1420A - Cubes Sorting

---
+++ 2022/2022-02-14/1420A_Cubes_Sorting.cpp
@@ -0,0 +1,5 @@
+// Problem: 1420A - Cubes Sorting
+// URL: https://codeforces.com/problemset/problem/1420/A
+// Date: 2022-02-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1420/submission/146298975
