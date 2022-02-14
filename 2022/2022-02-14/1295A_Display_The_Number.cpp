43017948ea89e8c7ac014a518a3bd43fff02222a
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-14 05:01:11 +0000

    solve: 1295A - Display The Number

---
+++ 2022/2022-02-14/1295A_Display_The_Number.cpp
@@ -0,0 +1,5 @@
+// Problem: 1295A - Display The Number
+// URL: https://codeforces.com/problemset/problem/1295/A
+// Date: 2022-02-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1295/submission/146305374
