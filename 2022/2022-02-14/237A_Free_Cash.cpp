ccd31c73ad7e9656f8bdf6bda1cb2df86995bc05
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-14 15:08:06 +0000

    solve: 237A - Free Cash

---
+++ 2022/2022-02-14/237A_Free_Cash.cpp
@@ -0,0 +1,5 @@
+// Problem: 237A - Free Cash
+// URL: https://codeforces.com/problemset/problem/237/A
+// Date: 2022-02-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/237/submission/146388102
