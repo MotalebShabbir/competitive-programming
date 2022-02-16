80a1533ea52ff9824396acbc1a6b308bac2490fd
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-16 09:05:35 +0000

    solve: 732B - Cormen --- The Best Friend Of a Man

---
+++ 2022/2022-02-16/732B_Cormen_---_The_Best_Friend_Of_a_Man.cpp
@@ -0,0 +1,5 @@
+// Problem: 732B - Cormen --- The Best Friend Of a Man
+// URL: https://codeforces.com/problemset/problem/732/B
+// Date: 2022-02-16
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/732/submission/146605350
