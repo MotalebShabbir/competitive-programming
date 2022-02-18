ff2351118185c6633c9cbf6553bdb9219d6eb3bc
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-18 09:35:18 +0000

    solve: 25A - IQ test

---
+++ 2022/2022-02-18/25A_IQ_test.cpp
@@ -0,0 +1,5 @@
+// Problem: 25A - IQ test
+// URL: https://codeforces.com/problemset/problem/25/A
+// Date: 2022-02-18
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/25/submission/146815357
