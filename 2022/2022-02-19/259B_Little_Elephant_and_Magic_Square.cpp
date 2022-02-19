89561b0e0fe683575305199000b0506013b7b037
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-19 08:07:07 +0000

    solve: 259B - Little Elephant and Magic Square

---
+++ 2022/2022-02-19/259B_Little_Elephant_and_Magic_Square.cpp
@@ -0,0 +1,5 @@
+// Problem: 259B - Little Elephant and Magic Square
+// URL: https://codeforces.com/problemset/problem/259/B
+// Date: 2022-02-19
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/259/submission/146907514
