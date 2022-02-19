aa34e3c641753df7553df9b89809aa5f0b473a73
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-19 06:18:17 +0000

    solve: 492B - Vanya and Lanterns

---
+++ 2022/2022-02-19/492B_Vanya_and_Lanterns.cpp
@@ -0,0 +1,5 @@
+// Problem: 492B - Vanya and Lanterns
+// URL: https://codeforces.com/problemset/problem/492/B
+// Date: 2022-02-19
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/492/submission/146898174
