5a3889a57756ca01c944aa7a2f09da51bb46b765
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-21 15:24:39 +0000

    solve: 466A - Cheap Travel

---
+++ 2022/2022-02-21/466A_Cheap_Travel.cpp
@@ -0,0 +1,5 @@
+// Problem: 466A - Cheap Travel
+// URL: https://codeforces.com/problemset/problem/466/A
+// Date: 2022-02-21
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/466/submission/147190736
