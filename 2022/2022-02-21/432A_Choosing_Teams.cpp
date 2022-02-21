08c0ad944b7e770314deccc0b97d3584907f5e4a
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-21 17:19:34 +0000

    solve: 432A - Choosing Teams

---
+++ 2022/2022-02-21/432A_Choosing_Teams.cpp
@@ -0,0 +1,5 @@
+// Problem: 432A - Choosing Teams
+// URL: https://codeforces.com/problemset/problem/432/A
+// Date: 2022-02-21
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/432/submission/147200701
