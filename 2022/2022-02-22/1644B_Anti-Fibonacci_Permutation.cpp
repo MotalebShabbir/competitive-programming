7718b92f78829bfad15b33858c14d65e34e2e640
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-22 15:14:01 +0000

    solve: 1644B - Anti-Fibonacci Permutation

---
+++ 2022/2022-02-22/1644B_Anti-Fibonacci_Permutation.cpp
@@ -0,0 +1,5 @@
+// Problem: 1644B - Anti-Fibonacci Permutation
+// URL: https://codeforces.com/problemset/problem/1644/B
+// Date: 2022-02-22
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1644/submission/147313518
