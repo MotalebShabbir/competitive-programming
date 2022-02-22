0c9f6049e4fd5025d5ed90ab34e3beb3b9e0bf7e
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-22 14:49:00 +0000

    solve: 1644A - Doors and Keys

---
+++ 2022/2022-02-22/1644A_Doors_and_Keys.cpp
@@ -0,0 +1,5 @@
+// Problem: 1644A - Doors and Keys
+// URL: https://codeforces.com/problemset/problem/1644/A
+// Date: 2022-02-22
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1644/submission/147294638
