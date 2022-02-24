db839a53503f24cce7bf4453f9c3a4c0662086d0
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-24 05:57:58 +0000

    solve: 709A - Juicer

---
+++ 2022/2022-02-24/709A_Juicer.cpp
@@ -0,0 +1,5 @@
+// Problem: 709A - Juicer
+// URL: https://codeforces.com/problemset/problem/709/A
+// Date: 2022-02-24
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/709/submission/147537501
