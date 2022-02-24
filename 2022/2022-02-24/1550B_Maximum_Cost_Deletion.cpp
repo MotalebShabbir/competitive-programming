5af89a73e297c0d4f70b02d13f90fbd8c2c6e035
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-24 09:34:55 +0000

    solve: 1550B - Maximum Cost Deletion

---
+++ 2022/2022-02-24/1550B_Maximum_Cost_Deletion.cpp
@@ -0,0 +1,5 @@
+// Problem: 1550B - Maximum Cost Deletion
+// URL: https://codeforces.com/problemset/problem/1550/B
+// Date: 2022-02-24
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1550/submission/147553508
