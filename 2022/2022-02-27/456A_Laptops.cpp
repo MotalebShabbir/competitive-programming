458208d655fec667e54d282992ab21e00874fe1f
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-27 04:44:04 +0000

    solve: 456A - Laptops

---
+++ 2022/2022-02-27/456A_Laptops.cpp
@@ -0,0 +1,5 @@
+// Problem: 456A - Laptops
+// URL: https://codeforces.com/problemset/problem/456/A
+// Date: 2022-02-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/456/submission/147822682
