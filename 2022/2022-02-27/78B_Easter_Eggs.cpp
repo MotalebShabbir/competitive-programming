cb5a502ad322718792042bbbae2d4c241b41047a
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-27 01:10:08 +0000

    solve: 78B - Easter Eggs

---
+++ 2022/2022-02-27/78B_Easter_Eggs.cpp
@@ -0,0 +1,5 @@
+// Problem: 78B - Easter Eggs
+// URL: https://codeforces.com/problemset/problem/78/B
+// Date: 2022-02-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/78/submission/147815687
