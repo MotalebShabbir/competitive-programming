f9fa3742be5da444d20500e291bc3993d4e60c12
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-27 02:58:13 +0000

    solve: 766A - Mahmoud and Longest Uncommon Subsequence

---
+++ 2022/2022-02-27/766A_Mahmoud_and_Longest_Uncommon_Subsequence.cpp
@@ -0,0 +1,5 @@
+// Problem: 766A - Mahmoud and Longest Uncommon Subsequence
+// URL: https://codeforces.com/problemset/problem/766/A
+// Date: 2022-02-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/766/submission/147818603
