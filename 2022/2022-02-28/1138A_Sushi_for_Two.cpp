f0bfd14d35ac2160185b7d7b49f0bb52eb1cb5c6
MotalebShabbir <motalebshabbir@gmail.com>
2022-02-28 05:04:05 +0000

    solve: 1138A - Sushi for Two

---
+++ 2022/2022-02-28/1138A_Sushi_for_Two.cpp
@@ -0,0 +1,5 @@
+// Problem: 1138A - Sushi for Two
+// URL: https://codeforces.com/problemset/problem/1138/A
+// Date: 2022-02-28
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1138/submission/147909712
