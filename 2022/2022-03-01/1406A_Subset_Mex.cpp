b46e9aa172dee46917719d67c6a266d1b1f492fb
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-01 13:33:30 +0000

    solve: 1406A - Subset Mex

---
+++ 2022/2022-03-01/1406A_Subset_Mex.cpp
@@ -0,0 +1,5 @@
+// Problem: 1406A - Subset Mex
+// URL: https://codeforces.com/problemset/problem/1406/A
+// Date: 2022-03-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1406/submission/148034643
