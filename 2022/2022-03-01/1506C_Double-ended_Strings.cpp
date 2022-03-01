905cdba793e4e57dc6d207fe6b173b73171d6db7
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-01 08:10:47 +0000

    solve: 1506C - Double-ended Strings

---
+++ 2022/2022-03-01/1506C_Double-ended_Strings.cpp
@@ -0,0 +1,5 @@
+// Problem: 1506C - Double-ended Strings
+// URL: https://codeforces.com/problemset/problem/1506/C
+// Date: 2022-03-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1506/submission/148008084
