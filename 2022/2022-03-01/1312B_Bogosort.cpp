03670faa1f6724b81f0f91d35b59606d6e39adcd
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-01 14:29:20 +0000

    solve: 1312B - Bogosort

---
+++ 2022/2022-03-01/1312B_Bogosort.cpp
@@ -0,0 +1,5 @@
+// Problem: 1312B - Bogosort
+// URL: https://codeforces.com/problemset/problem/1312/B
+// Date: 2022-03-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1312/submission/148039588
