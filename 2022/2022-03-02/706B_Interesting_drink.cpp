89f4cb192f74f7d53beca560c9a5dbdd5d3656d0
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-02 14:27:27 +0000

    solve: 706B - Interesting drink

---
+++ 2022/2022-03-02/706B_Interesting_drink.cpp
@@ -0,0 +1,5 @@
+// Problem: 706B - Interesting drink
+// URL: https://codeforces.com/problemset/problem/706/B
+// Date: 2022-03-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/706/submission/148124506
