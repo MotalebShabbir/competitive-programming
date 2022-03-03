ae3b6ba126a7b824f2b72a91c027841c125f005e
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-03 16:30:21 +0000

    solve: 1238A - Prime Subtraction

---
+++ 2022/2022-03-03/1238A_Prime_Subtraction.cpp
@@ -0,0 +1,5 @@
+// Problem: 1238A - Prime Subtraction
+// URL: https://codeforces.com/problemset/problem/1238/A
+// Date: 2022-03-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1238/submission/148219728
