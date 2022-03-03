28007cf3538ae432a22b38678d0196c8711fe487
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-03 01:43:34 +0000

    solve: 758A - Holiday Of Equality

---
+++ 2022/2022-03-03/758A_Holiday_Of_Equality.cpp
@@ -0,0 +1,5 @@
+// Problem: 758A - Holiday Of Equality
+// URL: https://codeforces.com/problemset/problem/758/A
+// Date: 2022-03-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/758/submission/148159108
