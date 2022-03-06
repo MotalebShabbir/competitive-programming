b0341ffe7c2839105b2acf0035bf1c201a3a1be0
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-06 10:30:41 +0000

    solve: 1515B - Phoenix and Puzzle

---
+++ 2022/2022-03-06/1515B_Phoenix_and_Puzzle.cpp
@@ -0,0 +1,5 @@
+// Problem: 1515B - Phoenix and Puzzle
+// URL: https://codeforces.com/problemset/problem/1515/B
+// Date: 2022-03-06
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1515/submission/148565356
