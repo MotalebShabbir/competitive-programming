c4132d2c292fd1fce4ece3f550c458fc8e3ffbc2
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-06 15:36:29 +0000

    solve: 490A - Team Olympiad

---
+++ 2022/2022-03-06/490A_Team_Olympiad.cpp
@@ -0,0 +1,5 @@
+// Problem: 490A - Team Olympiad
+// URL: https://codeforces.com/problemset/problem/490/A
+// Date: 2022-03-06
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/490/submission/148623286
