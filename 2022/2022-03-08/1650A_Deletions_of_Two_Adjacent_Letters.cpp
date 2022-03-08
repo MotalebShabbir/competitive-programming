ecbb7a8822f2565ef11dfd0eee07140d01dafc01
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-08 15:08:41 +0000

    solve: 1650A - Deletions of Two Adjacent Letters

---
+++ 2022/2022-03-08/1650A_Deletions_of_Two_Adjacent_Letters.cpp
@@ -0,0 +1,5 @@
+// Problem: 1650A - Deletions of Two Adjacent Letters
+// URL: https://codeforces.com/problemset/problem/1650/A
+// Date: 2022-03-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1650/submission/148846208
