5faf5c773b6d1cf76b33e0c6fc814e3cf21830c6
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-08 16:20:02 +0000

    solve: 1650B - DIV + MOD

---
+++ 2022/2022-03-08/1650B_DIV_+_MOD.cpp
@@ -0,0 +1,5 @@
+// Problem: 1650B - DIV + MOD
+// URL: https://codeforces.com/problemset/problem/1650/B
+// Date: 2022-03-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1650/submission/148894749
