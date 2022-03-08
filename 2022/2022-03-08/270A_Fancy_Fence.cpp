aeb099146ee8a4508cc0dc0e24e53e3bca6d5c73
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-08 13:16:51 +0000

    solve: 270A - Fancy Fence

---
+++ 2022/2022-03-08/270A_Fancy_Fence.cpp
@@ -0,0 +1,5 @@
+// Problem: 270A - Fancy Fence
+// URL: https://codeforces.com/problemset/problem/270/A
+// Date: 2022-03-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/270/submission/148800291
