60aa6da80a6871d709e30767cc4de98f87054f86
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-08 09:44:27 +0000

    solve: 1419D1 - Sage's Birthday (easy version)

---
+++ 2022/2022-03-08/1419D1_Sage's_Birthday_(easy_version).cpp
@@ -0,0 +1,5 @@
+// Problem: 1419D1 - Sage's Birthday (easy version)
+// URL: https://codeforces.com/problemset/problem/1419/D1
+// Date: 2022-03-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1419/submission/148780286
