acaa2b6df61f6e14929937a166f6a78702a78504
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-10 07:54:07 +0000

    solve: 1283B - Candies Division

---
+++ 2022/2022-03-10/1283B_Candies_Division.cpp
@@ -0,0 +1,5 @@
+// Problem: 1283B - Candies Division
+// URL: https://codeforces.com/problemset/problem/1283/B
+// Date: 2022-03-10
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1283/submission/149056650
