ff0ce5916f6d90ad037784e6e3c756434a743ac1
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-11 04:04:32 +0000

    solve: 1405B - Array Cancellation

---
+++ 2022/2022-03-11/1405B_Array_Cancellation.cpp
@@ -0,0 +1,5 @@
+// Problem: 1405B - Array Cancellation
+// URL: https://codeforces.com/problemset/problem/1405/B
+// Date: 2022-03-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1405/submission/149207229
