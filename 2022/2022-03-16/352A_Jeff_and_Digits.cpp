46c4d149fbfc1a03da3202a25ec2aa3f943362a4
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-16 14:31:37 +0000

    solve: 352A - Jeff and Digits

---
+++ 2022/2022-03-16/352A_Jeff_and_Digits.cpp
@@ -0,0 +1,5 @@
+// Problem: 352A - Jeff and Digits
+// URL: https://codeforces.com/problemset/problem/352/A
+// Date: 2022-03-16
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/352/submission/149828896
