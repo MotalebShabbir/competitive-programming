93d29909d639862a0b7c30e25720833b059f33c6
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-16 10:15:44 +0000

    solve: 1559B - Mocha and Red and Blue

---
+++ 2022/2022-03-16/1559B_Mocha_and_Red_and_Blue.cpp
@@ -0,0 +1,5 @@
+// Problem: 1559B - Mocha and Red and Blue
+// URL: https://codeforces.com/problemset/problem/1559/B
+// Date: 2022-03-16
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1559/submission/149801650
