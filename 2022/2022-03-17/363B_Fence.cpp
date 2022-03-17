5c30b24de95b7761d0ae9a64e52e17c4e2cf8111
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-17 04:58:28 +0000

    solve: 363B - Fence

---
+++ 2022/2022-03-17/363B_Fence.cpp
@@ -0,0 +1,5 @@
+// Problem: 363B - Fence
+// URL: https://codeforces.com/problemset/problem/363/B
+// Date: 2022-03-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/363/submission/149878235
