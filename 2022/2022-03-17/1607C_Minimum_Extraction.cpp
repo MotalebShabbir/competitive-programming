d2ed7b8ac3e95791bf68bbb3a0b83de0318d9a09
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-17 16:01:45 +0000

    solve: 1607C - Minimum Extraction

---
+++ 2022/2022-03-17/1607C_Minimum_Extraction.cpp
@@ -0,0 +1,5 @@
+// Problem: 1607C - Minimum Extraction
+// URL: https://codeforces.com/problemset/problem/1607/C
+// Date: 2022-03-17
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1607/submission/149936977
