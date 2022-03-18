3524454a7af3c01a6e6c60d97468d77d5001f606
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-18 23:06:02 +0000

    solve: 1139A - Even Substrings

---
+++ 2022/2022-03-18/1139A_Even_Substrings.cpp
@@ -0,0 +1,5 @@
+// Problem: 1139A - Even Substrings
+// URL: https://codeforces.com/problemset/problem/1139/A
+// Date: 2022-03-18
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1139/submission/150075763
