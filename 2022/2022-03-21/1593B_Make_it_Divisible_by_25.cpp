c67b126113905fb084f11e5697a1d59b8d239d16
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-21 09:52:51 +0000

    solve: 1593B - Make it Divisible by 25

---
+++ 2022/2022-03-21/1593B_Make_it_Divisible_by_25.cpp
@@ -0,0 +1,5 @@
+// Problem: 1593B - Make it Divisible by 25
+// URL: https://codeforces.com/problemset/problem/1593/B
+// Date: 2022-03-21
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1593/submission/150343250
