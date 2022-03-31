0aaa34d64d110b0efb7dfd9c27d74fa0c5abcdb0
MotalebShabbir <motalebshabbir@gmail.com>
2022-03-31 15:39:11 +0000

    solve: 1660A - Vasya and Coins

---
+++ 2022/2022-03-31/1660A_Vasya_and_Coins.cpp
@@ -0,0 +1,5 @@
+// Problem: 1660A - Vasya and Coins
+// URL: https://codeforces.com/problemset/problem/1660/A
+// Date: 2022-03-31
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1660/submission/151559480
