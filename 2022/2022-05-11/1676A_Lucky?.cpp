81b51905ec562169ef0d9658cb9a9ddc7c233055
MotalebShabbir <motalebshabbir@gmail.com>
2022-05-11 14:04:11 +0000

    solve: 1676A - Lucky?

---
+++ 2022/2022-05-11/1676A_Lucky?.cpp
@@ -0,0 +1,5 @@
+// Problem: 1676A - Lucky?
+// URL: https://codeforces.com/problemset/problem/1676/A
+// Date: 2022-05-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1676/submission/156839316
