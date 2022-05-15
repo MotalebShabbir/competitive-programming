cea2bc52365f896534e022bc4af07c6b17de689d
MotalebShabbir <motalebshabbir@gmail.com>
2022-05-15 10:39:57 +0000

    solve: 1679A - AvtoBus

---
+++ 2022/2022-05-15/1679A_AvtoBus.cpp
@@ -0,0 +1,5 @@
+// Problem: 1679A - AvtoBus
+// URL: https://codeforces.com/problemset/problem/1679/A
+// Date: 2022-05-15
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1679/submission/157293112
