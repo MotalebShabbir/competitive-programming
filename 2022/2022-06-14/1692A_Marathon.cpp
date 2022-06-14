2f80f69b015206d92c646e1f4b48058b03b4696c
MotalebShabbir <motalebshabbir@gmail.com>
2022-06-14 14:54:49 +0000

    solve: 1692A - Marathon

---
+++ 2022/2022-06-14/1692A_Marathon.cpp
@@ -0,0 +1,5 @@
+// Problem: 1692A - Marathon
+// URL: https://codeforces.com/problemset/problem/1692/A
+// Date: 2022-06-14
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1692/submission/160558128
