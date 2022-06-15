ad756988c3e742c40ccfb6e6579d0c6cce0bbabe
MotalebShabbir <motalebshabbir@gmail.com>
2022-06-15 06:58:49 +0000

    solve: 151A - Soft Drinking

---
+++ 2022/2022-06-15/151A_Soft_Drinking.cpp
@@ -0,0 +1,5 @@
+// Problem: 151A - Soft Drinking
+// URL: https://codeforces.com/problemset/problem/151/A
+// Date: 2022-06-15
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/151/submission/160700361
