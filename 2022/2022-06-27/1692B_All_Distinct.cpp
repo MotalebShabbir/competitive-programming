8b6b46a3b979037f7a382f77265ba22127d3edc7
MotalebShabbir <motalebshabbir@gmail.com>
2022-06-27 15:48:41 +0000

    solve: 1692B - All Distinct

---
+++ 2022/2022-06-27/1692B_All_Distinct.cpp
@@ -0,0 +1,5 @@
+// Problem: 1692B - All Distinct
+// URL: https://codeforces.com/problemset/problem/1692/B
+// Date: 2022-06-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1692/submission/161967552
