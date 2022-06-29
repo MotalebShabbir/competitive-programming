f91d5e0c03e6446af39bed87ff73e0884f650b3f
MotalebShabbir <motalebshabbir@gmail.com>
2022-06-29 08:27:36 +0000

    solve: 1692C - Where's the Bishop?

---
+++ 2022/2022-06-29/1692C_Where's_the_Bishop?.cpp
@@ -0,0 +1,5 @@
+// Problem: 1692C - Where's the Bishop?
+// URL: https://codeforces.com/problemset/problem/1692/C
+// Date: 2022-06-29
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1692/submission/162218232
