401a439059ceba87ccc227818ae058db768ee348
MotalebShabbir <motalebshabbir@gmail.com>
2022-07-01 12:03:27 +0000

    solve: 1692D - The Clock

---
+++ 2022/2022-07-01/1692D_The_Clock.cpp
@@ -0,0 +1,5 @@
+// Problem: 1692D - The Clock
+// URL: https://codeforces.com/problemset/problem/1692/D
+// Date: 2022-07-01
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1692/submission/162431841
