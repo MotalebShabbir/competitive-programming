57bcaeaf6eb17168e88ea9590be9042e341b2a9f
MotalebShabbir <motalebshabbir@gmail.com>
2022-07-08 02:32:58 +0000

    solve: 855A - Tom Riddle's Diary

---
+++ 2022/2022-07-08/855A_Tom_Riddle's_Diary.cpp
@@ -0,0 +1,5 @@
+// Problem: 855A - Tom Riddle's Diary
+// URL: https://codeforces.com/problemset/problem/855/A
+// Date: 2022-07-08
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/855/submission/163160791
