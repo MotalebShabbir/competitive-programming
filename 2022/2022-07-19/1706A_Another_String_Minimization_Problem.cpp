7ce12ac1fd4052803a0d6ca37622052da078c92f
MotalebShabbir <motalebshabbir@gmail.com>
2022-07-19 00:48:19 +0000

    solve: 1706A - Another String Minimization Problem

---
+++ 2022/2022-07-19/1706A_Another_String_Minimization_Problem.cpp
@@ -0,0 +1,5 @@
+// Problem: 1706A - Another String Minimization Problem
+// URL: https://codeforces.com/problemset/problem/1706/A
+// Date: 2022-07-19
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1706/submission/164829143
