5985daf0e7a77297b7a7ccd143942f5fc09cb63d
MotalebShabbir <motalebshabbir@gmail.com>
2022-07-21 14:56:36 +0000

    solve: 1709A - Three Doors

---
+++ 2022/2022-07-21/1709A_Three_Doors.cpp
@@ -0,0 +1,5 @@
+// Problem: 1709A - Three Doors
+// URL: https://codeforces.com/problemset/problem/1709/A
+// Date: 2022-07-21
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1709/submission/165153946
