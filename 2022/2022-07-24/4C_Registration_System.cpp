8905552202da10747be838bcf522f5a11ee9bc41
MotalebShabbir <motalebshabbir@gmail.com>
2022-07-24 09:16:30 +0000

    solve: 4C - Registration System

---
+++ 2022/2022-07-24/4C_Registration_System.cpp
@@ -0,0 +1,5 @@
+// Problem: 4C - Registration System
+// URL: https://codeforces.com/problemset/problem/4/C
+// Date: 2022-07-24
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/4/submission/165504940
