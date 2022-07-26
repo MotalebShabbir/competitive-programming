3e26d2ce2f24df28d9112d60e583521196cbc84d
MotalebShabbir <motalebshabbir@gmail.com>
2022-07-26 09:36:33 +0000

    solve: 799B - T-shirt buying

---
+++ 2022/2022-07-26/799B_T-shirt_buying.cpp
@@ -0,0 +1,5 @@
+// Problem: 799B - T-shirt buying
+// URL: https://codeforces.com/problemset/problem/799/B
+// Date: 2022-07-26
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/799/submission/165764088
