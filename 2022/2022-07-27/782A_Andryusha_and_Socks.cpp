7150ef35b986c8e9f58820f416e832ae84730858
MotalebShabbir <motalebshabbir@gmail.com>
2022-07-27 06:19:48 +0000

    solve: 782A - Andryusha and Socks

---
+++ 2022/2022-07-27/782A_Andryusha_and_Socks.cpp
@@ -0,0 +1,5 @@
+// Problem: 782A - Andryusha and Socks
+// URL: https://codeforces.com/problemset/problem/782/A
+// Date: 2022-07-27
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/782/submission/165854042
