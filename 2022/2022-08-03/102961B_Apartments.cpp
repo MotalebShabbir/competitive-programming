a7a9adcac58f069a15924ae806c92a6b27f074c8
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-03 06:30:15 +0000

    solve: 102961B - Apartments

---
+++ 2022/2022-08-03/102961B_Apartments.cpp
@@ -0,0 +1,5 @@
+// Problem: 102961B - Apartments
+// URL: https://codeforces.com/problemset/problem/102961/B
+// Date: 2022-08-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/102961/submission/166770579
