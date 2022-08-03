9bb38f6d0764754506a9af1191cc331e290e25ed
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-03 04:00:13 +0000

    solve: 102961A - Distinct Numbers

---
+++ 2022/2022-08-03/102961A_Distinct_Numbers.cpp
@@ -0,0 +1,5 @@
+// Problem: 102961A - Distinct Numbers
+// URL: https://codeforces.com/problemset/problem/102961/A
+// Date: 2022-08-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/102961/submission/166760946
