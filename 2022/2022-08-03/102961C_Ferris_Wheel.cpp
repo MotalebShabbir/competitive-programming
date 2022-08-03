4022b6e563686f26936eb88c65c0dd4c2e4f8099
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-03 08:04:20 +0000

    solve: 102961C - Ferris Wheel

---
+++ 2022/2022-08-03/102961C_Ferris_Wheel.cpp
@@ -0,0 +1,5 @@
+// Problem: 102961C - Ferris Wheel
+// URL: https://codeforces.com/problemset/problem/102961/C
+// Date: 2022-08-03
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/102961/submission/166779152
