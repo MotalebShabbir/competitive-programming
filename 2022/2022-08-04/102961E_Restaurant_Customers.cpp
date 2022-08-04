4e784a6295e403b58e104acd548e5f6834f3599e
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-04 03:17:38 +0000

    solve: 102961E - Restaurant Customers

---
+++ 2022/2022-08-04/102961E_Restaurant_Customers.cpp
@@ -0,0 +1,5 @@
+// Problem: 102961E - Restaurant Customers
+// URL: https://codeforces.com/problemset/problem/102961/E
+// Date: 2022-08-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/102961/submission/166871146
