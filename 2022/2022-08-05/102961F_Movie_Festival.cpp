d2dccb849c46bb998475bde8010938f5d1cfe753
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-05 16:19:26 +0000

    solve: 102961F - Movie Festival

---
+++ 2022/2022-08-05/102961F_Movie_Festival.cpp
@@ -0,0 +1,5 @@
+// Problem: 102961F - Movie Festival
+// URL: https://codeforces.com/problemset/problem/102961/F
+// Date: 2022-08-05
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/102961/submission/167130137
