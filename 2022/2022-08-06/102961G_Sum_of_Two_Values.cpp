172bd2a1bcfbf7a79c3318d7dc827f8b93becd40
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-06 06:06:56 +0000

    solve: 102961G - Sum of Two Values

---
+++ 2022/2022-08-06/102961G_Sum_of_Two_Values.cpp
@@ -0,0 +1,5 @@
+// Problem: 102961G - Sum of Two Values
+// URL: https://codeforces.com/problemset/problem/102961/G
+// Date: 2022-08-06
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/102961/submission/167183132
