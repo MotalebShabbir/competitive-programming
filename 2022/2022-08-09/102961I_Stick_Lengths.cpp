b6a36b5e8250e0f662561da1c0bb71635762a9e7
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-09 14:06:33 +0000

    solve: 102961I - Stick Lengths

---
+++ 2022/2022-08-09/102961I_Stick_Lengths.cpp
@@ -0,0 +1,5 @@
+// Problem: 102961I - Stick Lengths
+// URL: https://codeforces.com/problemset/problem/102961/I
+// Date: 2022-08-09
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/102961/submission/167644186
