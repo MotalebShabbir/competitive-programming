2cbc39c064b547c4486c9fff01b57b953aa323cc
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-10 09:27:56 +0000

    solve: 230B - T-primes

---
+++ 2022/2022-08-10/230B_T-primes.cpp
@@ -0,0 +1,5 @@
+// Problem: 230B - T-primes
+// URL: https://codeforces.com/problemset/problem/230/B
+// Date: 2022-08-10
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/230/submission/167730350
