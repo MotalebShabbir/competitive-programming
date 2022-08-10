e46b31aa310778096b4ec9d99d38ffd6c3c6441a
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-10 09:36:40 +0000

    solve: 1325A - EhAb AnD gCd

---
+++ 2022/2022-08-10/1325A_EhAb_AnD_gCd.cpp
@@ -0,0 +1,5 @@
+// Problem: 1325A - EhAb AnD gCd
+// URL: https://codeforces.com/problemset/problem/1325/A
+// Date: 2022-08-10
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1325/submission/167731296
