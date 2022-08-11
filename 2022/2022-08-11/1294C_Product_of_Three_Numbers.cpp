9ee131636ca02ac606a01a9b66e4234a028f175a
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-11 13:44:49 +0000

    solve: 1294C - Product of Three Numbers

---
+++ 2022/2022-08-11/1294C_Product_of_Three_Numbers.cpp
@@ -0,0 +1,5 @@
+// Problem: 1294C - Product of Three Numbers
+// URL: https://codeforces.com/problemset/problem/1294/C
+// Date: 2022-08-11
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1294/submission/167874716
