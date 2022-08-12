e3f56c7c9020aca0a0778795ffbdcba2e2c3dca5
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-12 09:20:01 +0000

    solve: 235A - LCM Challenge

---
+++ 2022/2022-08-12/235A_LCM_Challenge.cpp
@@ -0,0 +1,5 @@
+// Problem: 235A - LCM Challenge
+// URL: https://codeforces.com/problemset/problem/235/A
+// Date: 2022-08-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/235/submission/167954583
