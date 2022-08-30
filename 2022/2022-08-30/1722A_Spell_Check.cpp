c8a9439e710561823a2c90fd6cfb5d0d2184920e
MotalebShabbir <motalebshabbir@gmail.com>
2022-08-30 15:10:01 +0000

    solve: 1722A - Spell Check

---
+++ 2022/2022-08-30/1722A_Spell_Check.cpp
@@ -0,0 +1,5 @@
+// Problem: 1722A - Spell Check
+// URL: https://codeforces.com/problemset/problem/1722/A
+// Date: 2022-08-30
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1722/submission/170208850
