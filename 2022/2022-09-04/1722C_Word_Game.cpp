83e9c3bcaa945d7e5d494491790ce27bbd3f3ca1
MotalebShabbir <motalebshabbir@gmail.com>
2022-09-04 14:46:24 +0000

    solve: 1722C - Word Game

---
+++ 2022/2022-09-04/1722C_Word_Game.cpp
@@ -0,0 +1,5 @@
+// Problem: 1722C - Word Game
+// URL: https://codeforces.com/problemset/problem/1722/C
+// Date: 2022-09-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1722/submission/170858894
