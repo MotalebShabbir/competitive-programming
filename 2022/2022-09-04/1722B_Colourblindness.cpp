d2ee2af07f4d874b461e3888c75538f12a31a45b
MotalebShabbir <motalebshabbir@gmail.com>
2022-09-04 06:31:42 +0000

    solve: 1722B - Colourblindness

---
+++ 2022/2022-09-04/1722B_Colourblindness.cpp
@@ -0,0 +1,5 @@
+// Problem: 1722B - Colourblindness
+// URL: https://codeforces.com/problemset/problem/1722/B
+// Date: 2022-09-04
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1722/submission/170806674
