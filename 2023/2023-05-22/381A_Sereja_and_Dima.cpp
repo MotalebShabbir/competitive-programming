0d67e200a090822575e6f0d30302e623c9737b8c
MotalebShabbir <motalebshabbir@gmail.com>
2023-05-22 15:18:00 +0000

    solve: 381A - Sereja and Dima

---
+++ 2023/2023-05-22/381A_Sereja_and_Dima.cpp
@@ -0,0 +1,5 @@
+// Problem: 381A - Sereja and Dima
+// URL: https://codeforces.com/problemset/problem/381/A
+// Date: 2023-05-22
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/381/submission/206855721
