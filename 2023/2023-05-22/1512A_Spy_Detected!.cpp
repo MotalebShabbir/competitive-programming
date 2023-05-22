201dea04a8e4ce24278eb18facf2ebd05cc58d0b
MotalebShabbir <motalebshabbir@gmail.com>
2023-05-22 11:09:46 +0000

    solve: 1512A - Spy Detected!

---
+++ 2023/2023-05-22/1512A_Spy_Detected!.cpp
@@ -0,0 +1,5 @@
+// Problem: 1512A - Spy Detected!
+// URL: https://codeforces.com/problemset/problem/1512/A
+// Date: 2023-05-22
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1512/submission/206823506
