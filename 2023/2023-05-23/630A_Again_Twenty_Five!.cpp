d773ff7555ef48c05a20f519a3b0255cbe058305
MotalebShabbir <motalebshabbir@gmail.com>
2023-05-23 08:37:01 +0000

    solve: 630A - Again Twenty Five!

---
+++ 2023/2023-05-23/630A_Again_Twenty_Five!.cpp
@@ -0,0 +1,5 @@
+// Problem: 630A - Again Twenty Five!
+// URL: https://codeforces.com/problemset/problem/630/A
+// Date: 2023-05-23
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/630/submission/206918668
