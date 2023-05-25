c337fb90faefe029ca22bf658926f5e383709702
MotalebShabbir <motalebshabbir@gmail.com>
2023-05-25 04:01:30 +0000

    solve: 189A - Cut Ribbon

---
+++ 2023/2023-05-25/189A_Cut_Ribbon.cpp
@@ -0,0 +1,5 @@
+// Problem: 189A - Cut Ribbon
+// URL: https://codeforces.com/problemset/problem/189/A
+// Date: 2023-05-25
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/189/submission/207100297
