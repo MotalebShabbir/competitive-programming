5630b18061bf54aae47b778b140a63985b97f0ab
MotalebShabbir <motalebshabbir@gmail.com>
2023-06-12 15:31:43 +0000

    solve: 1841A - Game with Board

---
+++ 2023/2023-06-12/1841A_Game_with_Board.cpp
@@ -0,0 +1,5 @@
+// Problem: 1841A - Game with Board
+// URL: https://codeforces.com/problemset/problem/1841/A
+// Date: 2023-06-12
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1841/submission/209439109
