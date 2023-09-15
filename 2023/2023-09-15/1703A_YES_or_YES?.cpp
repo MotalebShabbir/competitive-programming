9cc3f76ebe51b5197553b2bcfb988ffb2908fd15
MotalebShabbir <motalebshabbir@gmail.com>
2023-09-15 02:52:54 +0000

    solve: 1703A - YES or YES?

---
+++ 2023/2023-09-15/1703A_YES_or_YES?.cpp
@@ -0,0 +1,5 @@
+// Problem: 1703A - YES or YES?
+// URL: https://codeforces.com/problemset/problem/1703/A
+// Date: 2023-09-15
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1703/submission/223425340
