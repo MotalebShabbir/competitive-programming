632df77399dde90b17490337ccc14905a1e2974e
MotalebShabbir <motalebshabbir@gmail.com>
2023-10-02 17:00:21 +0000

    solve: 1742A - Sum

---
+++ 2023/2023-10-02/1742A_Sum.cpp
@@ -0,0 +1,5 @@
+// Problem: 1742A - Sum
+// URL: https://codeforces.com/problemset/problem/1742/A
+// Date: 2023-10-02
+// Language: C++17 (GCC 9-64)
+// Submission: https://codeforces.com/contest/1742/submission/226330445
