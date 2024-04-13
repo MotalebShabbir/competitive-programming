89be2f1818622a3296d16f694654cb6fd1209e52
MotalebShabbir <motalebshabbir@gmail.com>
2024-04-13 16:47:22 +0000

    solve: 1955A - Yogurt Sale

---
+++ 2024/2024-04-13/1955A_Yogurt_Sale.cpp
@@ -0,0 +1,5 @@
+// Problem: 1955A - Yogurt Sale
+// URL: https://codeforces.com/problemset/problem/1955/A
+// Date: 2024-04-13
+// Language: C++17 (GCC 7-32)
+// Submission: https://codeforces.com/contest/1955/submission/256539797
