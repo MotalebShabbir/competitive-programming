7037ad928eac7b233d344dc22298d5d2f2d177a6
MotalebShabbir <motalebshabbir@gmail.com>
2024-05-26 11:57:00 +0000

    solve: 1971A - My First Sorting Problem

---
+++ 2024/2024-05-26/1971A_My_First_Sorting_Problem.cpp
@@ -0,0 +1,5 @@
+// Problem: 1971A - My First Sorting Problem
+// URL: https://codeforces.com/problemset/problem/1971/A
+// Date: 2024-05-26
+// Language: C++17 (GCC 7-32)
+// Submission: https://codeforces.com/contest/1971/submission/262699647
