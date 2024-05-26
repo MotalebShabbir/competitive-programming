dd74a8e6c536a899a2c539362935d7b75193a7cf
MotalebShabbir <motalebshabbir@gmail.com>
2024-05-26 14:22:42 +0000

    solve: 1971B - Different String

---
+++ 2024/2024-05-26/1971B_Different_String.cpp
@@ -0,0 +1,5 @@
+// Problem: 1971B - Different String
+// URL: https://codeforces.com/problemset/problem/1971/B
+// Date: 2024-05-26
+// Language: C++17 (GCC 7-32)
+// Submission: https://codeforces.com/contest/1971/submission/262715931
