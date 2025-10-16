22b1f2a21f500128c89194fbe8d09c3c0ffe392b
MotalebShabbir <motalebshabbir@gmail.com>
2025-10-16 10:51:06 +0000

    solve: 1999A - A+B Again?

---
+++ 2025/2025-10-16/1999A_A+B_Again?.py
@@ -0,0 +1,5 @@
+// Problem: 1999A - A+B Again?
+// URL: https://codeforces.com/problemset/problem/1999/A
+// Date: 2025-10-16
+// Language: Python 3
+// Submission: https://codeforces.com/contest/1999/submission/344009712
