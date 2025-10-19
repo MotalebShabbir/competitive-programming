16ec40de50efe1948004cb86631b834ba4e15883
MotalebShabbir <motalebshabbir@gmail.com>
2025-10-19 18:52:44 +0000

    solve: 1791A - Codeforces Checking

---
+++ 2025/2025-10-19/1791A_Codeforces_Checking.cpp
@@ -0,0 +1,5 @@
+// Problem: 1791A - Codeforces Checking
+// URL: https://codeforces.com/problemset/problem/1791/A
+// Date: 2025-10-19
+// Language: C++17 (GCC 7-32)
+// Submission: https://codeforces.com/contest/1791/submission/344765048
